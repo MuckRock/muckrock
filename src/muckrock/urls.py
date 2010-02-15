d2ea4d6645ed610d8c88457119ece9a9b213e311
Mitchell Kotler <mitch@mitchellkotler.net>
2010-02-15 18:05:53 -0500

    Password reset feature (work on issue #1)

--- src/muckrock/urls.py
+++ src/muckrock/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 
 from django.views.generic.simple import direct_to_template
-from django.contrib.auth.views import login, logout, password_change, password_change_done
+from django.contrib.auth.views import login, logout, password_change, password_change_done, \
+    password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
 from views import register, update
 import settings
@@ -10,17 +11,21 @@ import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$',                   direct_to_template, {'template': 'home.html'}),
+    (r'^$', direct_to_template, {'template': 'home.html'}),
 
-    (r'^accounts/login/$',     login),
-    (r'^accounts/logout/$',    logout),
-    (r'^accounts/profile/$',   direct_to_template, {'template': 'registration/profile.html'}),
-    (r'^accounts/register/$',  register),
-    (r'^accounts/update/$',    update),
-    (r'^accounts/change_pw/$', password_change),
-    (r'^accounts/change_pw_done/$', password_change_done),
+    (r'^accounts/login/$',             login),
+    (r'^accounts/logout/$',            logout),
+    (r'^accounts/profile/$',           direct_to_template, {'template': 'registration/profile.html'}),
+    (r'^accounts/register/$',          register),
+    (r'^accounts/update/$',            update),
+    (r'^accounts/change_pw/$',         password_change),
+    (r'^accounts/change_pw_done/$',    password_change_done),
+    (r'^accounts/reset_pw/$',          password_reset),
+    (r'^accounts/reset_pw_done/$',     password_reset_done),
+    (r'^accounts/reset_pw_complete/$', password_reset_complete),
+    (r'^accounts/reset_pw/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',  password_reset_confirm),
 
-    (r'^admin/',              include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
