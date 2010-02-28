82ecf6c1a3a7685a9961bf89c44b60b8338a3eba
Mitchell Kotler <mitch@mitchellkotler.net>
2010-02-27 20:15:57 -0500

    pretty urls for foia requests (issue #22)

--- src/muckrock/foia/forms.py
+++ src/muckrock/foia/forms.py
@@ -4,9 +4,25 @@ Forms for FOIA application
 
 from django import forms
 from foia.models import FOIARequest
+from django.template.defaultfilters import slugify
 
 class FOIARequestForm(forms.ModelForm):
     """A form for a FOIA Request"""
+
+    def clean(self):
+        """Check user and slug are unique together"""
+
+        forms.ModelForm.clean(self)
+
+        user = self.instance.user
+        slug = slugify(self.cleaned_data['title'])
+
+        other_foia = FOIARequest.objects.filter(user=user, slug=slug)
+
+        if len(other_foia) != 0:
+            raise forms.ValidationError('You already have a FOIA request with a similar title')
+
+        return self.cleaned_data
 
     class Meta:
         # pylint: disable-msg=R0903
--- src/muckrock/foia/urls.py
+++ src/muckrock/foia/urls.py
@@ -8,13 +8,14 @@ from django.views.generic import list_detail
 from foia import views
 from foia.models import FOIARequest
 
-foia_request_qs = {'queryset': FOIARequest.objects.all()}
+foia_qs = {'queryset': FOIARequest.objects.all()}
 
 urlpatterns = patterns('',
-    url(r'^view/(?P<object_id>\d+)/$',        list_detail.object_detail, foia_request_qs,
-        name='foia-view'),
-    (r'^list/$',                              list_detail.object_list, foia_request_qs),
-    (r'^list/(?P<user_name>[A-Za-z0-9_]+)/$', views.list_by_user),
+    (r'^list/$',                              list_detail.object_list, foia_qs),
+    (r'^list/(?P<user_name>[A-Za-z0-9_i]+)/$', views.list_by_user),
     (r'^new/$',                               views.create),
-    (r'^update/(?P<object_id>\d+)/$',         views.update),
+    url(r'^view/(?P<user_name>[A-Za-z0-9_]+)/(?P<slug>[A-Za-z0-9_-]+)/$',
+                                              views.detail),
+    (r'^update/(?P<user_name>[A-Za-z0-9_]+)/(?P<slug>[A-Za-z0-9_-]+)/$',
+                                              views.update),
 )
