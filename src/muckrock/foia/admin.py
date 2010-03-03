c2ecc4ce3ff906e67d6237cc1d93aef59717e7da
Mitchell Kotler <mitch@mitchellkotler.net>
2010-03-02 23:05:10 -0500

    Customize admin for foia (closing issue #31)

--- src/muckrock/foia/admin.py
+++ src/muckrock/foia/admin.py
@@ -5,5 +5,13 @@ Admin registration for FOIA models
 from django.contrib import admin
 from foia.models import FOIARequest
 
-admin.site.register(FOIARequest)
+class FOIARequestAdmin(admin.ModelAdmin):
+    """FOIA Request admin options"""
+    # pylint: disable-msg=R0904
+
+    list_display = ('title', 'user', 'status', 'jurisdiction')
+    list_filter = ['status', 'jurisdiction']
+    search_fields = ['title', 'request', 'response']
+
+admin.site.register(FOIARequest, FOIARequestAdmin)
 
