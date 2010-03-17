a5780c21a1e0fa51404cbd9bab45ea52aed10686
Mitchell Kotler <mitch@mitchellkotler.net>
2010-03-17 15:36:45 -0400

    new design for demo (messed up last check in and pushed)

--- src/muckrock/news/urls.py
+++ src/muckrock/news/urls.py
@@ -12,13 +12,16 @@ article_date_args = dict(article_args, date_field='pub_date')
 article_date_list_args = dict(article_date_args, allow_empty=True)
 
 urlpatterns = patterns('',
-        url(r'^$', list_detail.object_list, dict(article_args, paginate_by=5), name='news-index'),
-        url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[-\w\d]+)/$',
+        url(r'^$', date_based.archive_index, dict(article_date_list_args, num_latest=5)),
+
+        url(r'^archives/$', list_detail.object_list,
+            dict(article_args, paginate_by=25), name='news-index'),
+        url(r'^archives/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[-\w\d]+)/$',
             date_based.object_detail, article_date_args, name='news-detail'),
-        url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$',
+        url(r'^archives/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$',
             date_based.archive_day, article_date_list_args),
-        url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
+        url(r'^archives/(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
             date_based.archive_month, article_date_list_args),
-        url(r'^(?P<year>\d{4})/$',
+        url(r'^archives/(?P<year>\d{4})/$',
             date_based.archive_year, dict(article_date_list_args, make_object_list=True)),
 )
--- src/muckrock/templatetags/templatetags/tags.py
+++ src/muckrock/templatetags/templatetags/tags.py
@@ -9,6 +9,7 @@ register = template.Library()
 
 @register.simple_tag
 def active(request, pattern):
+    """Check url against pattern to determine active css attribute"""
     pattern = pattern.replace('{{user}}', str(request.user))
     if re.search(pattern, request.path):
         return 'current-tab'
