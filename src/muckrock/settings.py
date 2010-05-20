53aaece4cbb0ad7fb4bde6201103150f5e4d9279
Mitchell Kotler <mitch@mitchellkotler.net>
2010-05-20 17:09:02 -0400

    update to support the csrf in django 1.2

--- src/muckrock/settings.py
+++ src/muckrock/settings.py
@@ -74,6 +74,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfResponseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
