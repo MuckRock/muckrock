fbb8b50d553fc4b7af4fdd24768d35a890050b19
Mitchell Kotler <mitch@mitchellkotler.net>
2010-06-09 17:01:49 -0400

    update date range, closing #57

--- src/muckrock/static/js/datepicker.js
+++ src/muckrock/static/js/datepicker.js
@@ -3,7 +3,8 @@
 		$('.datepicker').datepicker({
 			changeMonth: true,
 			changeYear: true,
-			minDate: new Date(1900, 0, 1),
-			maxDate: +0
+			minDate: new Date(1776, 6, 4),
+			maxDate: +0,
+			yearRange: '1776:+00'
 		});
 	});
