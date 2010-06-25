bc8d86a873f236b72c527b908ceb2f9122bdda93
Mitchell Kotler <mitch@mitchellkotler.net>
2010-06-25 12:45:53 -0400

    FOIA urls include id, remove unique constraint
    Closes issue #59

--- src/muckrock/foia/urls.py
+++ src/muckrock/foia/urls.py
@@ -3,8 +3,8 @@ URL mappings for the FOIA application
 """
 
 from django.conf.urls.defaults import patterns, url
-from django.views.generic import list_detail
 from django.contrib.auth.decorators import login_required
+from django.views.generic import list_detail
 
 from foia import views
 from foia.models import FOIARequest
@@ -13,6 +13,8 @@ from foia.feeds import LatestSubmittedRequests, LatestDoneRequests
 
 foia_qs = {'queryset': FOIARequest.objects.all(),
            'paginate_by': 10}
+
+foia_url = r'(?P<jurisdiction>[\w\d_-]+)/(?P<slug>[\w\d_-]+)/(?P<idx>\d+)'
 
 urlpatterns = patterns('',
     url(r'^list/$',                     list_detail.object_list, foia_qs, name='foia-list'),
@@ -23,16 +25,12 @@ urlpatterns = patterns('',
     url(r'^new/$',                      login_required(foia_wizard),
                                         kwargs={'extra_context': wizard_extra_context},
                                         name='foia-create'),
-    url(r'^view/(?P<jurisdiction>[\w\d_-]+)/(?P<user_name>[\w\d_]+)/(?P<slug>[\w\d_-]+)/$',
-                                        views.detail, name='foia-detail'),
-    url(r'^view/(?P<jurisdiction>[\w\d_-]+)/(?P<user_name>[\w\d_]+)/(?P<slug>[\w\d_-]+)/'
-         'doc/(?P<page>\d+)/$',
+    url(r'^view/%s/$' % foia_url,       views.detail, name='foia-detail'),
+    url(r'^view/%s/doc/(?P<page>\d+)/$' % foia_url,
                                         views.document_detail, name='foia-doc-detail'),
     url(r'^update/$',                   views.update_list, name='foia-update-list'),
-    url(r'^update/(?P<jurisdiction>[\w\d\_-]+)/(?P<user_name>[\w\d_]+)/(?P<slug>[\w\d_-]+)/$',
-                                        views.update, name='foia-update'),
-    url(r'^delete/(?P<jurisdiction>[\w\d_-]+)/(?P<user_name>[\w\d_]+)/(?P<slug>[\w\d_-]+)/$',
-                                        views.delete, name='foia-delete'),
+    url(r'^update/%s/$' % foia_url,     views.update, name='foia-update'),
+    url(r'^delete/%s/$' % foia_url,     views.delete, name='foia-delete'),
     url(r'^feeds/submitted/$',          LatestSubmittedRequests(), name='foia-submitted-feed'),
     url(r'^feeds/completed/$',          LatestDoneRequests(), name='foia-done-feed'),
 )
