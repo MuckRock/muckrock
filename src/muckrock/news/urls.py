8d7d44f4e9300722754b646d757da466bdd25cc3
Mitchell Kotler <mitch@mitchellkotler.net>
2010-08-06 17:22:49 -0400

    make news public

--- src/muckrock/news/urls.py
+++ src/muckrock/news/urls.py
@@ -4,7 +4,6 @@ URL mappings for the News application
 
 from django.conf.urls.defaults import patterns, url
 from django.views.generic import list_detail, date_based
-from django.contrib.auth.decorators import login_required
 
 from news.models import Article
 from news.feeds import LatestEntries
@@ -16,23 +15,23 @@ article_date_list_args = dict(article_date_args, allow_empty=True)
 years = [date.year for date in Article.objects.dates('pub_date', 'year')][::-1]
 
 urlpatterns = patterns('',
-        url(r'^$', login_required(date_based.archive_index),
+        url(r'^$', date_based.archive_index,
             dict(article_date_list_args, num_latest=5),
             name='news-index'),
-        url(r'^archives/$', login_required(list_detail.object_list),
+        url(r'^archives/$', list_detail.object_list,
             dict(article_args, paginate_by=10, extra_context={'years': years}),
             name='news-archive'),
         url(r'^archives/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[-\w\d]+)/$',
-            login_required(date_based.object_detail), article_date_args, name='news-detail'),
+            date_based.object_detail, article_date_args, name='news-detail'),
         url(r'^archives/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$',
-            login_required(date_based.archive_day),
+            date_based.archive_day,
             article_date_list_args, name='news-archive-day'),
         url(r'^archives/(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
-            login_required(date_based.archive_month),
+            date_based.archive_month,
             article_date_list_args, name='news-archive-month'),
         url(r'^archives/(?P<year>\d{4})/$',
-            login_required(date_based.archive_year),
+            date_based.archive_year,
             dict(article_date_list_args, make_object_list=True),
             name='news-archive-year'),
-        url(r'^feeds/$', login_required(LatestEntries()), name='news-feed'),
+        url(r'^feeds/$', LatestEntries(), name='news-feed'),
 )
