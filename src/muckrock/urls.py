eac1972325d9d40b2db2e56e312ff9dbc5d9f73d
Mitchell Kotler <mitch@mitchellkotler.net>
2010-08-11 23:40:36 -0400

    change url from foia to foi

--- src/muckrock/urls.py
+++ src/muckrock/urls.py
@@ -30,7 +30,7 @@ sitemaps = {'FOIA': FoiaSitemap, 'News': ArticleSitemap}
 urlpatterns = patterns('',
     url(r'^$', archive_index, article_args, name='index'),
     url(r'^accounts/', include(accounts.urls)),
-    url(r'^foia/', include(foia.urls)),
+    url(r'^foi/', include(foia.urls)),
     url(r'^news/', include(news.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^search/', include(haystack.urls)),
