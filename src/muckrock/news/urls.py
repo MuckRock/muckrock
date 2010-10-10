668470402ce74f22fd3666f45446ef04889d9dfe
Mitchell Kotler <mitch@mitchellkotler.net>
2010-10-10 15:26:31 -0400

    #131 staff users can view unpublished articles on the site in order to preview them

--- src/muckrock/news/urls.py
+++ src/muckrock/news/urls.py
@@ -5,12 +5,12 @@ URL mappings for the News application
 from django.conf.urls.defaults import patterns, url
 from django.views.generic import list_detail, date_based
 
+from news import views
 from news.models import Article
 from news.feeds import LatestEntries
 
 article_args = {'queryset': Article.objects.get_published()}
-article_date_args = dict(article_args, date_field='pub_date')
-article_date_list_args = dict(article_date_args, allow_empty=True)
+article_date_list_args = dict(article_args, date_field='pub_date', allow_empty=True)
 
 years = [date.year for date in Article.objects.dates('pub_date', 'year')][::-1]
 
@@ -21,7 +21,7 @@ urlpatterns = patterns('',
             dict(article_args, paginate_by=10, extra_context={'years': years}),
             name='news-archive'),
         url(r'^archives/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[-\w\d]+)/$',
-            date_based.object_detail, article_date_args, name='news-detail'),
+            views.news_detail, name='news-detail'),
         url(r'^archives/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$',
             date_based.archive_day, article_date_list_args, name='news-archive-day'),
         url(r'^archives/(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
