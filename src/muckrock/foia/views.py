1e654aaa0f6b534f652a67a46ce367b8693e5889
Mitchell Kotler <mitch@mitchellkotler.net>
2011-03-28 23:29:03 -0400

    dont add footer to request and remove phone number #196 and #197

--- src/muckrock/foia/views.py
+++ src/muckrock/foia/views.py
@@ -65,15 +65,6 @@ def _foia_form_handler(request, foia, action):
                 foia_comm = foia.communications.all()[0]
                 foia_comm.date = datetime.now()
                 foia_comm.communication = form.cleaned_data['request']
-                if request.POST['submit'] == 'Submit Request':
-                    foia_comm.communication += \
-                        '\nFiled via MuckRock.com\n' \
-                        'E-mail (Preferred): requests@muckrock.com\n' \
-                        'Daytime: (617) 299-1832\n' \
-                        'For mailed responses, please address:\n' \
-                        'MuckRock\n' \
-                        '185 Beacon St. #3\n' \
-                        'Somerville, MA 02143'
                 foia_comm.save()
                 foia.save()
 
