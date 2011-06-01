00cffe7b237b067029e039c318d8d981bce5cfdc
Mitchell Kotler <mitch@mitchellkotler.net>
2011-05-31 21:58:40 -0400

    nicer 500 page to reference sentry ids

--- src/muckrock/urls.py
+++ src/muckrock/urls.py
@@ -4,7 +4,8 @@ URL mappings for muckrock project
 
 # pylint: disable-msg=W0611
 # these are called dynmically
-from django.conf.urls.defaults import handler404, handler500
+from django.conf.urls.defaults import handler404
+from views import handler500
 # pylint: enable-msg=W0611
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
