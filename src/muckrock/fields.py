63dc9e28346cce30b6e9e324ab320524ab90409a
Mitchell Kotler <mitch@mitchellkotler.net>
2011-05-31 22:09:35 -0400

    merge top level fields and forms

--- src/muckrock/fields.py
+++ src/muckrock/fields.py
@@ -3,10 +3,12 @@ Custom fields
 """
 
 import re
+from itertools import groupby
 
 from django.core.validators import email_re
 from django.db.models import CharField
 from django.forms import Textarea, ValidationError
+from django.forms.models import ModelChoiceIterator, ModelChoiceField
 from django.utils.translation import ugettext as _
 from south.modelsinspector import add_introspection_rules
 
@@ -17,6 +19,7 @@ email_separator_re = re.compile(r'[^\w\.\-\+@_]+')
 def _is_valid_email(email):
     """Validates an email address"""
     return email_re.match(email)
+
 
 class EmailsListField(CharField):
     """Multi email field"""
@@ -38,3 +41,48 @@ class EmailsListField(CharField):
                 raise ValidationError(_('%s is not a valid e-mail address.') % email)
 
         return value
+
+
+class GroupedModelChoiceField(ModelChoiceField):
+    """Form field for grouped model choice"""
+    def __init__(self, group_by_field, group_label=None, *args, **kwargs):
+        """
+        group_by_field is the name of a field on the model
+        group_label is a function to return a label for each choice group
+        """
+        super(GroupedModelChoiceField, self).__init__(*args, **kwargs)
+        self.group_by_field = group_by_field
+        if group_label is None:
+            self.group_label = lambda group: group
+        else:
+            self.group_label = group_label
+
+    def _get_choices(self):
+        """
+        Exactly as per ModelChoiceField except returns new iterator class
+        """
+        if hasattr(self, '_choices'):
+            return self._choices
+        return GroupedModelChoiceIterator(self)
+    choices = property(_get_choices, ModelChoiceField._set_choices)
+
+
+class GroupedModelChoiceIterator(ModelChoiceIterator):
+    """Iterator for grouped model choice"""
+    # pylint: disable-msg=R0903
+    def __iter__(self):
+        if self.field.empty_label is not None:
+            yield (u"", self.field.empty_label)
+        if self.field.cache_choices:
+            if self.field.choice_cache is None:
+                self.field.choice_cache = [
+                    (self.field.group_label(group), [self.choice(ch) for ch in choices])
+                        for group,choices in groupby(self.queryset.all(),
+                            key=lambda row: getattr(row, self.field.group_by_field))
+                ]
+            for choice in self.field.choice_cache:
+                yield choice
+        else:
+            for group, choices in groupby(self.queryset.all(),
+                    key=lambda row: getattr(row, self.field.group_by_field)):
+                yield (self.field.group_label(group), [self.choice(ch) for ch in choices])
