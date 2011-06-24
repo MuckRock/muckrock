66c2eafb8549946f592d20aea01f8fa05124307b
Mitchell Kotler <mitch@mitchellkotler.net>
2011-06-23 23:52:56 -0400

    log lamson errors to sentry

--- src/automailer/config/boot.py
+++ src/automailer/config/boot.py
@@ -9,7 +9,24 @@ import logging
 import logging.config
 import jinja2
 
+from sentry.client.handlers import SentryHandler
+
 logging.config.fileConfig("config/logging.conf")
+
+logger = logging.getLogger()
+rlogger = logging.getLogger('routing')
+sentry_handler = SentryHandler()
+sentry_handler.setLevel(logging.INFO)
+
+# ensure we havent already registered the handler
+if SentryHandler not in [x.__class__ for x in logger.handlers]:
+    logger.addHandler(sentry_handler)
+    rlogger.addHandler(sentry_handler)
+
+    # Add StreamHandler to sentry's default so you can catch missed exceptions
+    logger = logging.getLogger('sentry.errors')
+    logger.propagate = False
+    logger.addHandler(logging.StreamHandler())
 
 # the relay host to actually send the final message to
 settings.relay = Relay(host=settings.relay_config['host'],
