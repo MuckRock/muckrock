0c530adf2affac2d80ccb424a9ae7af478c12bee
Mitchell Kotler <mitch@mitchellkotler.net>
2011-10-17 21:48:04 -0400

    #260 - admin fix is set as being from the owner of the request

--- src/muckrock/foia/views.py
+++ src/muckrock/foia/views.py
@@ -173,7 +173,7 @@ def _save_foia_comm(request, foia, form, action):
         foia.email = form.cleaned_data['email']
         foia.other_emails = form.cleaned_data['other_emails']
     FOIACommunication.objects.create(
-            foia=foia, from_who=request.user.get_full_name(), to_who=foia.get_to_who(),
+            foia=foia, from_who=foia.user.get_full_name(), to_who=foia.get_to_who(),
             date=datetime.now(), response=False, full_html=False,
             communication=form.cleaned_data['comm'])
     foia.submit(appeal=(action == 'Appeal'))
