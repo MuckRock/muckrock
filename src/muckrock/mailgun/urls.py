c6d6492712d580118f4fdf21f8d8bfe7500f1c26
Mitchell Kotler <mitch@mitchellkotler.net>
2011-12-18 23:58:26 -0500

    initial mailgun app

---
+++ src/muckrock/mailgun/urls.py
@@ -0,0 +1,12 @@
+"""
+URL mappings for mailgun
+"""
+
+from django.conf.urls.defaults import patterns, url
+
+from mailgun import views
+
+urlpatterns = patterns('',
+        url(r'^request/(?P<mail_id>\d+-\d{8})/$', views.handle_request, name='mailgun-request'),
+        url(r'^fax/$',                            views.fax, name='mailgun-fax'),
+)
