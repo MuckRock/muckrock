c8ce35c6abd26005d59e51f5e03c36f1ae1512ea
Mitchell Kotler <mitch@mitchellkotler.net>
2012-07-09 23:42:51 -0400

    remove view and move idx to end

--- muckrock/agency/urls.py
+++ muckrock/agency/urls.py
@@ -8,13 +8,12 @@ from django.views.generic.simple import redirect_to
 from agency import views
 from muckrock.views import jurisdiction
 
-agency_url = r'(?P<jurisdiction>[\w\d_-]+)/(?P<idx>\d+)-(?P<slug>[\w\d_-]+)'
+agency_url = r'(?P<jurisdiction>[\w\d_-]+)/(?P<slug>[\w\d_-]+)-(?P<idx>\d+)'
 old_agency_url = r'(?P<jurisdiction>[\w\d_-]+)/(?P<slug>[\w\d_-]+)/(?P<idx>\d+)'
 
 urlpatterns = patterns('',
     url(r'^$',                        views.list_, name='agency-list'),
-    url(r'^%s/$' % agency_url,        redirect_to, {'url': 'view'}, name='agency-default'),
-    url(r'^%s/view/$' % agency_url,   views.detail, name='agency-detail'),
+    url(r'^%s/$' % agency_url,        views.detail, name='agency-detail'),
     url(r'^%s/update/$' % agency_url, views.update, name='agency-update'),
     url(r'^%s/flag/$' % agency_url,   views.flag, name='agency-flag'),
     url(r'^(?P<jurisdiction>[\w\d_-]+)/$',
@@ -22,9 +21,9 @@ urlpatterns = patterns('',
 
     # old urls for redirects
     url(r'^view/%s/$' % old_agency_url,
-        redirect_to, {'url': '/agency/%(jurisdiction)s/%(idx)s-%(slug)s/view/'}),
-    url(r'^update/%s/$' % old_agency_url,
-        redirect_to, {'url': '/agency/%(jurisdiction)s/%(idx)s-%(slug)s/update/'}),
-    url(r'^flag/%s/$' % old_agency_url,
-        redirect_to, {'url': '/agency/%(jurisdiction)s/%(idx)s-%(slug)s/flag/'}),
+        redirect_to, {'url': '/agency/%(jurisdiction)s/%(slug)s-%(idx)s/'}),
+    url(r'^update/%s/$' % old_agency_url,                              
+        redirect_to, {'url': '/agency/%(jurisdiction)s/%(slug)s-%(idx)s/update/'}),
+    url(r'^flag/%s/$' % old_agency_url,                                
+        redirect_to, {'url': '/agency/%(jurisdiction)s/%(slug)s-%(idx)s/flag/'}),
 )
--- muckrock/jurisdiction/urls.py
+++ muckrock/jurisdiction/urls.py
@@ -3,7 +3,6 @@ URL mappings for the jurisdiction application
 """
 
 from django.conf.urls.defaults import patterns, url
-from django.views.generic.simple import redirect_to
 
 from jurisdiction import views
 from muckrock.views import jurisdiction
@@ -13,10 +12,8 @@ old_jur_url = r'(?P<slug>[\w\d_-]+)/(?P<idx>\d+)'
 
 urlpatterns = patterns('',
     url(r'^$',                   views.list_, name='jurisdiction-list'),
-    url(r'^%s/view/$' % jur_url, views.detail, name='jurisdiction-detail'),
     url(r'^%s/flag/$' % jur_url, views.flag, name='jurisdiction-flag'),
-    url(r'^%s/$' % jur_url,      redirect_to, {'url': 'view'}, name='jurisdiction-default'),
-
+    url(r'^%s/$' % jur_url,      views.detail, name='jurisdiction-detail'),
 )
 
 # old url patterns go under jurisdictions, new ones switched to places
