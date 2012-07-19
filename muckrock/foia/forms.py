e24092f823638bfddeba6d0c29b781e9a1898384
Mitchell Kotler <mitch@mitchellkotler.net>
2012-07-18 20:39:18 -0400

    fix empty slug bug

--- muckrock/foia/forms.py
+++ muckrock/foia/forms.py
@@ -579,8 +579,9 @@ class FOIAWizard(DynamicSessionFormWizard):
 
         if len(title) > 70:
             title = title[:70]
+        slug = slugify(title) or 'untitled'
         foia = FOIARequest.objects.create(user=request.user, status='started', title=title,
-                                          jurisdiction=jurisdiction, slug=slugify(title),
+                                          jurisdiction=jurisdiction, slug=slug,
                                           agency=agency)
         FOIACommunication.objects.create(
                 foia=foia, from_who=request.user.get_full_name(), to_who=foia.get_to_who(),
@@ -593,7 +594,7 @@ class FOIAWizard(DynamicSessionFormWizard):
         return HttpResponseRedirect(reverse('foia-update',
                                     kwargs={'jurisdiction': jurisdiction.slug,
                                             'idx': foia.id,
-                                            'slug': slugify(title)}))
+                                            'slug': slug}))
 
     def process_step(self, form):
         """Process each step"""
