630eb763c45bf175d8947f0f35aae4e619087d76
Mitchell Kotler <mitch@mitchellkotler.net>
2012-08-15 22:22:32 -0400

    #367 fix for last check in - add evaluate tag

--- muckrock/templatetags/templatetags/tags.py
+++ muckrock/templatetags/templatetags/tags.py
@@ -2,6 +2,7 @@
 General temaplate tags
 """
 
+from django import template
 from django.template import Library, Node, TemplateSyntaxError
 from django.template.defaultfilters import stringfilter
 
@@ -133,3 +134,31 @@ def redact_emails(text):
     """Redact emails from text"""
     return email_re.sub(email_redactor, text)
 
+# http://stackoverflow.com/questions/1278042/
+# in-django-is-there-an-easy-way-to-render-a-text-field-as-a-template-in-a-templ/1278507#1278507
+@register.tag(name="evaluate")
+def do_evaluate(parser, token):
+    """
+    tag usage {% evaluate object.textfield %}
+    """
+    # pylint: disable=W0613
+    try:
+        _, variable = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError, \
+            "%r tag requires a single argument" % token.contents.split()[0]
+    return EvaluateNode(variable)
+
+class EvaluateNode(template.Node):
+    """Node for do_evaluate"""
+    def __init__(self, variable):
+        # pylint: disable=W0231
+        self.variable = template.Variable(variable)
+
+    def render(self, context):
+        try:
+            content = self.variable.resolve(context)
+            tmpl = template.Template(content)
+            return tmpl.render(context)
+        except (template.VariableDoesNotExist, template.TemplateSyntaxError):
+            return 'Error rendering', self.variable
