1aead9e1c863c5278e632b580fe8936d52e2a614
Mitchell Kotler <mitch@mitchellkotler.net>
2013-02-19 08:47:22 -0500

    more q&a

--- muckrock/qanda/urls.py
+++ muckrock/qanda/urls.py
@@ -4,13 +4,18 @@ URL mappings for the Q&A application
 
 from django.conf.urls.defaults import patterns, url
 from django.views.generic.list_detail import object_list, object_detail
+from django.views.generic.create_update import create_object
 
 from qanda.models import Question
+from qanda.forms import QuestionForm, AnswerForm
 
 article_args = {'queryset': Question.objects.get_published()}
 
 urlpatterns = patterns('',
         url(r'^$', object_list, {'queryset': Question.objects.all()}, name='question-index'),
-        url(r'(?P<slug>[\w\d_-]+)-(?P<object_id>\d+)^$', object_detail,
+        url(r'^new/$', create_object, {'form_class': QuestionForm}, name= 'question-create'),
+        url(r'^(?P<slug>[\w\d_-]+)-(?P<object_id>\d+)$', object_detail,
             {'queryset': Question.objects.all()}, name='question-detail'),
+        url(r'^(?P<slug>[\w\d_-]+)-(?P<object_id>\d+)/answer$', create_object,
+            {'form_class': AnswerForm}, name='answer-create'),
 )
