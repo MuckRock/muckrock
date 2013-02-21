0571fe5d42e832628fef63cdc703b9a5b91d9711
Mitchell Kotler <mitch@mitchellkotler.net>
2013-02-20 20:37:48 -0500

    order users alphabetically #391

--- muckrock/agency/admin.py
+++ muckrock/agency/admin.py
@@ -2,7 +2,9 @@
 Admin registration for Agency models
 """
 
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 from agency.models import AgencyType, Agency
 
@@ -15,12 +17,22 @@ class AgencyTypeAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 
+class AgencyAdminForm(forms.ModelForm):
+    """Agency admin form to order users"""
+    user = forms.models.ModelChoiceField(queryset=User.objects.all().order_by('username'))
+
+    class Meta:
+        # pylint: disable=R0903
+        model = Agency
+
+
 class AgencyAdmin(admin.ModelAdmin):
     """Agency admin options"""
     prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'jurisdiction')
     list_filter = ['approved', 'jurisdiction', 'types']
     search_fields = ['name']
+    form = AgencyAdminForm
 
 admin.site.register(AgencyType, AgencyTypeAdmin)
 admin.site.register(Agency,     AgencyAdmin)
