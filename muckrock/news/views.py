a221af1d90497ccf3227cb78ba9a7a79c1488d21
Mitchell Kotler <mitch@mitchellkotler.net>
2013-03-10 14:33:55 -0400

    update to django 1.4.5
    Conflicts:
    	muckrock/foia/views.py
    	requirements.txt

--- muckrock/news/views.py
+++ muckrock/news/views.py
@@ -2,29 +2,52 @@
 Views for the news application
 """
 
-from django.views.generic import date_based
+from django.views.generic.list import ListView
+from django.views.generic.dates import YearArchiveView, DateDetailView
 
 from muckrock.news.models import Article
 
-def news_detail(request, year, month, day, slug):
+# pylint: disable=R0901
+
+class NewsDetail(DateDetailView):
     """View for news detail"""
+    date_field = 'pub_date'
 
-    if request.user.is_staff:
-        queryset = Article.objects.all()
-        allow_future = True
-    else:
-        queryset = Article.objects.get_published()
-        allow_future = False
+    def get_queryset(self):
+        """Get articles for this view"""
+        if self.request.user.is_staff:
+            return Article.objects.all()
+        else:
+            return Article.objects.get_published()
 
-    return date_based.object_detail(request, year, month, day, queryset, 'pub_date',
-                                    slug=slug, allow_future=allow_future)
+    def get_allow_future(self):
+        """Can future posts be seen?"""
+        return self.request.user.is_staff
 
-def news_year(request, **kwargs):
+
+class NewsYear(YearArchiveView):
     """View for year archive"""
-    year = int(kwargs['year'])
-    extra_context = {}
-    if year > 1000:
-        extra_context['prev_year'] = year - 1
-    if year < 9999:
-        extra_context['next_year'] = year + 1
-    return date_based.archive_year(request, extra_context=extra_context, **kwargs)
+    allow_empty = True
+    date_field = 'pub_date'
+    make_object_list = True
+    queryset = Article.objects.get_published()
+
+    def get_context_data(self, **kwargs):
+        context = super(NewsYear, self).get_context_data(**kwargs)
+        year = int(self.kwargs['year'])
+        if year > 1000:
+            context['prev_year'] = year - 1
+        if year < 9999:
+            context['next_year'] = year + 1
+        return context
+
+
+class List(ListView):
+    """List of news articles"""
+    paginate_by = 10
+    queryset = Article.objects.get_published()
+
+    def get_context_data(self, **kwargs):
+        context = super(List, self).get_context_data(**kwargs)
+        context['years'] = [date.year for date in Article.objects.dates('pub_date', 'year')][::-1]
+        return context
