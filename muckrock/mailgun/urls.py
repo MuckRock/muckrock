b8210f4d407d424f72e1f8d9ee2c3d830358f8e9
Mitchell Kotler <mitch@mitchellkotler.net>
2014-02-24 19:46:26 -0500

    fix mailgun urls

--- muckrock/mailgun/urls.py
+++ muckrock/mailgun/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.defaults import patterns, url
 from muckrock.mailgun import views
 
 urlpatterns = patterns('',
-        url(r'^request/(?P<mail_id>\d+-\d{8})/$', views.handle_request, name='mailgun-request'),
-        url(r'^fax/$',                            views.fax, name='mailgun-fax'),
-        url(r'^bounces/$',                        views.bounces, name='mailgun-bounces'),
+        url(r'^request/(?P<mail_id>\d+-\d{3,10})/$', views.handle_request, name='mailgun-request'),
+        url(r'^fax/$',                               views.fax, name='mailgun-fax'),
+        url(r'^bounces/$',                           views.bounces, name='mailgun-bounces'),
 )
