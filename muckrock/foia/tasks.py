cb2b6a3f0cbf2ee433eb3e73c34c34c4e94ba7d4
Mitchell Kotler <mitch@mitchellkotler.net>
2014-03-08 15:19:17 -0500

    log followups for #530

--- muckrock/foia/tasks.py
+++ muckrock/foia/tasks.py
@@ -192,13 +192,16 @@ def followup_requests():
     """Follow up on any requests that need following up on"""
     # change to this after all follows up have been resolved
     #for foia in FOIARequest.objects.get_followup(): 
-    logger.info('foia.tasks.followup_requests task being run')
+    log = []
     if options.enable_followup:
         foia_requests = FOIARequest.objects.filter(status__in=['ack', 'processed'],
                                                    date_followup__lte=date.today())
-        logger.info('%d requests to follow up on', foia_requests.count())
         for foia in foia_requests:
+            log.append('%s - %s' % (foia.status, foia.title))
             foia.followup()
+
+        send_mail('[LOG] Follow Ups', '\n'.join(log), 'info@muckrock.com',
+                  ['requests@muckrock.com', 'mitch@muckrock.com'])
 
 
 @periodic_task(run_every=crontab(hour=6, minute=0), name='muckrock.foia.tasks.embargo_warn')
