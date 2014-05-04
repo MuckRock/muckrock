3e63771ab9d8c200cc96bcd3b4b3e2432aa6b971
Mitchell Kotler <mitch@mitchellkotler.net>
2014-05-04 11:00:52 -0400

    #567 enable secure cookies for csrf also

--- muckrock/settings.py
+++ muckrock/settings.py
@@ -22,6 +22,7 @@ AWS_DEBUG = False
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ADMINS = (
     ('Mitchell Kotler', 'mitch@muckrock.com'),
