aa69d8f2a30b17e24cbf721a5dc23da4f9ea75b8
Mitchell Kotler <mitch@mitchellkotler.net>
2014-05-04 12:34:32 -0400

    #545 res-c marks requests as complete

--- muckrock/foia/codes.py
+++ muckrock/foia/codes.py
@@ -6,7 +6,7 @@ CODES = {
 	'ACK': ('Acknowledgement Letter', 'processed', 'An acknowledgement letter, stating the request is being processed.'),
 	'NRD': ('No Responsive Documents', 'no_docs', 'A no responsive documents response.'),
 	'NRD-T': ('No Responsive Documents', 'no_docs', 'Potentially responsive documents were transferred to another department.'),
-	'RES-C': ('Cover Letter', 'processed', 'A cover letter granting the request and outlining any exempted materials, if any.'),
+	'RES-C': ('Cover Letter', 'done', 'A cover letter granting the request and outlining any exempted materials, if any.'),
 	'RES-P': ('Partial Responsive Documents', 'partial', 'A first set of responsive documents from the agency, with more to be sent at a specified date.'),
 	'RES': ('Responsive Documents', 'done', 'A copy of documents responsive to the request.'),
 	'FEE-R': ('Fee Waiver Rejected', 'payment', 'A letter stating the request for reduced or waived fees has been rejected.'),
