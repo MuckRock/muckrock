3031b025b3925fcfef6bf6411a255c0cb1d4b484
Mitchell Kotler <mitch@mitchellkotler.net>
2014-06-02 21:36:16 -0400

    #595 agency api fields

--- muckrock/agency/serializers.py
+++ muckrock/agency/serializers.py
@@ -11,9 +11,28 @@ from muckrock.agency.models import Agency
 class AgencySerializer(serializers.ModelSerializer):
     """Serializer for Agency model"""
     types = serializers.RelatedField(many=True)
+
+    def __init__(self, *args, **kwargs):
+        # pylint: disable=E1101
+        # pylint: disable=E1002
+        super(AgencySerializer, self).__init__(*args, **kwargs)
+
+        if 'request' not in self.context:
+            self.fields.pop('email')
+            self.fields.pop('other_emails')
+            return
+
+        request = self.context['request']
+
+        if not request.user.is_staff:
+            self.fields.pop('email')
+            self.fields.pop('other_emails')
+
     class Meta:
         model = Agency
         fields = ('id', 'name', 'slug', 'jurisdiction', 'types', 'public_notes', 'address',
                   'contact_salutation', 'contact_first_name', 'contact_last_name',
-                  'contact_title', 'url', 'expires', 'phone', 'fax')
+                  'contact_title', 'url', 'expires', 'phone', 'fax', 'approved', 'appeal_agency',
+                  'can_email_appeals', 'image_attr_line', 'stale', 'website', 'twitter',
+                  'twitter_handles', 'foia_logs', 'foia_guide', 'exempt', 'email', 'other_emails')
 
