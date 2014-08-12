ac1b91761d99106e5e819129a537411ef51375be
Allan Lasser <lasser.allan@gmail.com>
2014-08-12 14:55:33 -0400

    began implementing a template for the redesign

--- muckrock/settings.py
+++ muckrock/settings.py
@@ -162,6 +162,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT, 'new-template'),
     os.path.join(SITE_ROOT, 'templates'),
 )
 
