88ef833cfe3e0a04d73c9e9e8677da68a3c6a546
Allan Lasser <lasser.allan@gmail.com>
2014-10-02 16:33:46 -0400

    basic foundation for new request system

--- muckrock/foia/new_forms.py
+++ muckrock/foia/new_forms.py
@@ -3,71 +3,41 @@ import autocomplete_light as autocomplete
 from muckrock.jurisdiction.models import Jurisdiction
 from muckrock.agency.models import Agency
 
-class DocumentForm(forms.Form):
+class RequestForm(forms.Form):
+    # form data
+    JURISDICTION_CHOICES = [
+        ('f', 'Federal'),
+        ('s', 'State'),
+        ('l', 'Local')
+    ]
+
+    # form fields
     title = forms.CharField()
     document = forms.CharField(widget=forms.Textarea(attrs = {'placeholder': 'One sentence describing the specific document you are after.'}))
-
-class JurisdictionForm(forms.Form):
-    states = Jurisdiction.objects.filter(level='s', hidden=False)
-    localities = Jurisdiction.objects.filter(level='l', hidden=False)
-    
-    STATES_CHOICES = [('', '--Pick a State--')] + [(s.abbrev, s.name) for s in states]
-    
-    is_federal = forms.BooleanField(required=False)
-    is_state = forms.BooleanField(required=False)
-    is_local = forms.BooleanField(required=False)
-    state = forms.ChoiceField(
-        choices=STATES_CHOICES,
+    jurisdiction = forms.ChoiceField(choices=JURISDICTION_CHOICES)
+    state = autocomplete.ModelChoiceField(
+        'StateAutocomplete',
+        queryset=Jurisdiction.objects.filter(level='s', hidden=False), 
         required=False
     )
     local = autocomplete.ModelChoiceField(
         'LocalAutocomplete',
-        queryset=localities.order_by('parent', 'name'),
+        queryset=Jurisdiction.objects.filter(level='l', hidden=False).order_by('parent', 'name'),
         required=False
     )
-    
-    def clean(self):
-        """Ensures conditional fields are filled in"""
-        data = self.cleaned_data
-        is_federal = data.get('is_federal')
-        is_state, is_local = data.get('is_state'), data.get('is_local')
-        state, local = data.get('state'), data.get('local')
-        if not is_federal and not is_state and not is_local:
-            error_msg = 'No jurisdiction was selected'
-            self._errors['federal'] = self.error_class([error_msg])
-        if is_state and not state:
-            error_msg = 'No state was selected'
-            self._errors['state'] = self.error_class([error_msg])
-        if not is_state and is_local and not local:
-            error_msg = 'No locality was selected'
-            self._errors['local'] = self.error_class([error_msg])
-        return self.cleaned_data
-        
-class AgencyForm(forms.Form):
-
-    other = forms.CharField(
+    agency = forms.ModelChoiceField(
+        label='Agency',
         required=False,
-        help_text='Separate agency names with commas'
+        widget=forms.Select(attrs={'class': 'combobox'}),
+        help_text=('Select one of the agencies for the jurisdiction you '
+                   'have chosen, or write in the correct agency if known.')
     )
     
     def clean(self):
-        """Ensures at least one agency is chosen"""
-        agencies = [key for key, value in self.cleaned_data.items() if key is not 'other' and value is not False]
-        if not agencies:
-            error_msg = 'You must add at least one agency.'
-            self._errors['agencies'] = self.error_class([error_msg])
-        return self.cleaned_data
-  
-    def __init__(self, *args, **kwargs):
-        try:
-            initial = kwargs.pop('initial')
-            jurisdictions = initial['jurisdictions']
-        except KeyError as e:
-            print 'KeyError: ' + e
-        super(AgencyForm, self).__init__(*args, **kwargs)
+        if jurisdiction == 's' and not state:
+            error_msg = 'No state was selected'
+            self._errors['state'] = self.error_class([error_msg])
+        if jurisdiction == 'l' and not local:
+            error_msg = 'No locality was selected'
+            self._errors['local'] = self.error_class([error_msg])
         
-        agencies = []      
-        for jurisdiction in jurisdictions:
-            agencies += Agency.objects.filter(jurisdiction=jurisdiction)
-        for agency in agencies:
-            self.fields[agency.name] = forms.BooleanField(required=False)
--- muckrock/foia/views/new_views.py
+++ muckrock/foia/views/new_views.py
@@ -6,9 +6,10 @@ from django.shortcuts import render_to_response, redirect
 from django.template.defaultfilters import slugify
 from django.template.loader import render_to_string, get_template
 from django.template import RequestContext
+from django.utils import simplejson
 
 from muckrock.agency.models import Agency
-import muckrock.foia.new_forms as forms
+from muckrock.foia.new_forms import RequestForm
 from muckrock.foia.models import FOIARequest, FOIACommunication
 from muckrock.jurisdiction.models import Jurisdiction
 
@@ -17,92 +18,66 @@ from datetime import datetime
 import logging
 logger = logging.getLogger(__name__)
 
-FORMS = [
-    ('document', forms.DocumentForm),
-    ('jurisdiction', forms.JurisdictionForm),
-    ('agency', forms.AgencyForm)
-]
-
-TEMPLATES = {
-    'document': 'foia/create/document.html',
-    'jurisdiction': 'foia/create/jurisdiction.html',
-    'agency': 'foia/create/agency.html',
-    'fallback': 'foia/create/base_create.html'
-}
-
 SESSION_NAME = 'foia_request'
 
-class RequestWizard(SessionWizardView):
-    
-    jurisdiction = [] 
-    
-    def _get_jurisdiction_list(self):
-        """Creates a list of all chosen jurisdictions"""
-        j_list = []
-        data = self.get_cleaned_data_for_step('jurisdiction')
-        is_state, is_local = data['is_state'], data['is_local']
-        state, local = data['state'], data['local']
-        if data.get('is_federal'):
-            j_list += Jurisdiction.objects.filter(level='f', hidden=False)
-        if is_state:
-            the_state = Jurisdiction.objects.filter(level='s', abbrev=state)
-            j_list += the_state
-            if is_local and not local:
-                s_id = the_state[0].id
-                j_list += Jurisdiction.objects.filter(level='l', parent=s_id)
-        if is_local:
-            j_list += Jurisdiction.objects.filter(level='l', full_name=local)
-        args = {'jurisdictions': j_list}
-        self.jurisdiction = j_list
-        return args
-    
-    def _save_summary(self):
-        doc_input = self.get_cleaned_data_for_step('document')
-        agency_input = self.get_cleaned_data_for_step('agency')
-        user = self.request.user if not self.request.user.is_anonymous() \
-                                 else False
-        title = doc_input['title']
-        document = doc_input['document']
-        new_agencies = agency_input['other'].split(',')
-        for i, j in enumerate(new_agencies):
-            new_agencies[i] = j.lstrip()
-            if not new_agencies[i]: # cleans out empty strings from array
-                new_agencies.remove(new_agencies[i])
-        agencies = [key for key, value in agency_input.items() if key != 'other' and value != False]
-        for i, agency in enumerate(agencies):
-            agencies[i] = (Agency.objects.filter(name=agency))[0]
-            # the .filter() function returns a QuerySet,
-            # so the first element of that set is taken as the agency
-        args = {
-            'title': title,
-            'document': document,
-            'jurisdiction': self.jurisdiction,
-            'agencies': agencies,
-            'new_agencies': new_agencies
-        }
-        self.request.session[SESSION_NAME] = pickle.dumps(args)
+def create_request(request):
+    initial_data = {}
+    clone = False
+    if request.session.get(SESSION_NAME, False):
+        clone = True
+        print request.session[SESSION_NAME]
+
+        # TODO drop session data into initial_data
         
-    def get_form_initial(self, step):
-        initial = self.initial_dict.get(step, {})
-        args = {}
-        if step == 'agency':
-            args = self._get_jurisdiction_list()
-        initial.update(args)
-        return initial
+    if request.method == 'GET':
+        results = []
+        if request.GET.has_key(u'jID'):
+            j_id = request.GET[u'jID']
+            agencies = Agency.objects.filter(jurisdiction=jID).order_by('name')
+            results += [agency.name for agency in agencies]
+        json = simplejson.dumps(results)
+        return HttpResponse(json, mimetype='application/json')
+    elif request.method == 'POST':
+        form = RequestForm(request.POST)
+        # drop the data into SESSION_NAME
+        if form.is_valid():
+            title = form.title
+            document = form.document
+            if form.jurisdiction == 'f':
+                jurisdiction = Jurisdiction.objects.filter(level='f')
+            elif form.jurisdiction == 's':
+                jurisdiction = form.state
+            else:
+                jurisdiction = form.locality
+            agency = Agency.objects.filter(name=form.agency)
+            is_new_agency = False
+            if not agency:
+                agency = form.agency
+                is_new_agency = True
+            request.session[SESSION_ID] = {
+                'title': title,
+                'document': document,
+                'jurisdiction': jurisdiction,
+                'agency': agency,
+                'is_new_agency': is_new_agency,
+                'is_clone': clone
+            }
+    else:
+        if clone:
+            form = RequestForm(initial=initial_data)
+        else:
+            form = RequestForm()
     
-    def get_template_names(self):
-        return [TEMPLATES[self.steps.current], TEMPLATES['fallback']]
+    def render_to_response(
+        'forms/foia/create.html',
+        {'form': form, 'clone': clone},
+        context_instance=RequestContext(request)
+    )
     
-    def done(self, form_list, **kwargs):
-        try:
-            self._save_summary()
-        except pickle.PicklingError as e:
-            print e
-        return redirect('foia-submit')
         
 def submit_request(request):
         
-    def _compose_preview(document, agencies):
+    def _compose_comm(document, jurisdiction):
         intro = 'This is a request under the Freedom of Information Act.'
         waiver = ('I also request that, if appropriate, fees be waived as I '
                   'believe this request is in the public interest. '
@@ -113,14 +88,12 @@ def submit_request(request):
                   ' process of news gathering and not for commercial usage.')
         delay = '20 business days'
         
-        if len(agencies) == 1:
-            j = agencies[0].jurisdiction
-            if j.get_intro():
-                intro = j.get_intro()                
-            if j.get_waiver():
-                waiver = j.get_waiver()
-            if j.get_days():
-                delay = j.get_days()
+        if jurisdiction.get_intro():
+            intro = jurisdiction.get_intro()                
+        if jurisdiction.get_waiver():
+            waiver = jurisdiction.get_waiver()
+        if jurisdiction.get_days():
+            delay = jurisdiction.get_days()
         
         prepend = [intro + ' I hereby request the following records:']
         append = [waiver,
@@ -135,16 +108,17 @@ def submit_request(request):
         return prepend + [document] + append
         
     def _create_request(foia):
-        slug = slugify(foia['title']) or 'untitled'
-        jurisdiction = foia['jurisdiction'][0]
-        if foia['agencies']:
-            agency = foia['agencies'][0]
-            is_new_agency = False
-        else:
-            new_agency = foia['new_agencies'][0]
+        title = foia['title']
+        document = foia['document']
+        slug = slugify(title) or 'untitled'
+        jurisdiction = foia['jurisdiction']
+        agency = foia['agency']
+        is_new_agency = foia['is_new_agency']
+        is_clone = foia['is_clone']
+        if is_new_agency:
             agency = Agency.objects.create(
                 name=new_agency[:255],
-                slug=(slugify(new_agency[:255]) or 'untitled'),
+                slug=(slugify(agency[:255]) or 'untitled'),
                 jurisdiction=jurisdiction,
                 user=request.user,
                 approved=False
@@ -159,16 +133,15 @@ def submit_request(request):
                 ['requests@muckrock.com'],
                 fail_silently=False
             )
-            is_new_agency = True
         foia = FOIARequest.objects.create(
             user=request.user,
             status='started',
-            title=foia['title'],
+            title=title,
             jurisdiction=jurisdiction,
             slug=slug,
             agency=agency,
-            requested_docs=foia['document'],
-            description=foia['document']
+            requested_docs=document,
+            description=document
         )
         FOIACommunication.objects.create(
             foia=foia,
@@ -177,7 +150,7 @@ def submit_request(request):
             date=datetime.now(),
             response=False,
             full_html=False,
-            communication=request_text
+            communication=_compose_comm(document, jurisdiction)
         )
         foia_comm = foia.communications.all()[0]
         foia_comm.date = datetime.now()
@@ -188,19 +161,15 @@ def submit_request(request):
             foia_request = pickle.loads(request.session[SESSION_NAME])
         except pickle.UnpicklingError as e:
             print e
+            return redirect('index')
     else:
         return redirect('index')
-        
-    agency_names = [agency.name for agency in foia_request['agencies']] + \
-                    foia_request['new_agencies']
-    request_text = _compose_preview(foia_request['document'],
-                                    foia_request['agencies'])
     
     if request.method == 'POST':
         command = request.POST.get('submit', False)
         if command:
             if command == 'Submit' or command == 'Save Draft':
-                foia, foia_comm, new_agency = _create_request(foia_request)
+                foia, foia_comm, is_new_agency = _create_request(foia_request)
                 if command == 'Submit':
                     foia.status = 'submitted'
                 if request.user.get_profile().make_request():
@@ -218,7 +187,7 @@ def submit_request(request):
 
                 del request.session[SESSION_NAME]
 
-                if new_agency:
+                if is_new_agency:
                     args = {
                         'jurisdiction': foia.agency.jurisdiction.slug,
                         'jidx': foia.agency.jurisdiction.pk,
@@ -236,9 +205,14 @@ def submit_request(request):
     
     context = {
         'title': foia_request['title'],
-        'agency_names': agency_names,
-        'request_text': request_text
+        'agency': foia_request['agency'],
+        'jurisdiction': foia_request['jurisdiction'],
+        'comm': _compose_comm(
+            foia_request['document'],
+            foia_request['jurisdiction']
+        ),
+        'is_clone': foia_request['is_clone']
     }
 
-    return render_to_response('foia/create/confirm.html', context, 
+    return render_to_response('forms/foia/confirm.html', context, 
                               context_instance=RequestContext(request))
