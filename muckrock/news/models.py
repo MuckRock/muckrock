d92f2452879bc8489aa94eb66a110b285b794ff7
Allan Lasser <lasser.allan@gmail.com>
2014-11-13 18:01:09 -0500

    updates styling and layout of news articles

--- muckrock/news/models.py
+++ muckrock/news/models.py
@@ -36,12 +36,23 @@ class Article(models.Model):
     body = models.TextField('Body text')
     authors = models.ManyToManyField(User, related_name='authored_articles')
     editors = models.ManyToManyField(User, related_name='edited_articles', blank=True, null=True)
-    publish = models.BooleanField('Publish on site', default=False,
-            help_text='Articles will not appear on the site until their "publish date".')
-    foias = models.ManyToManyField(FOIARequest, related_name='articles', blank=True, null=True)
-    image = ThumbnailerImageField(upload_to='news_images', blank=True, null=True,
-                                  resize_source={'size': (1200, 675), 'crop': 'smart'})
-
+    publish = models.BooleanField(
+        'Publish on site',
+        default=False,
+        help_text='Articles do not appear on the site until their publish date.'
+    )
+    foias = models.ManyToManyField(
+        FOIARequest,
+        related_name='articles',
+        blank=True,
+        null=True
+    )
+    image = ThumbnailerImageField(
+        upload_to='news_images',
+        blank=True,
+        null=True,
+        resize_source={'size': (1200, 675), 'crop': 'smart'}
+    )
     objects = ArticleManager()
     tags = TaggableManager(through=TaggedItemBase, blank=True)
 
