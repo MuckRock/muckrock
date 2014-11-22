2a6bb6b6b06a5012c14577b4daf51d92c3ebc145
Mitchell Kotler <mitch@mitchellkotler.net>
2014-11-22 09:20:07 -0500

    more perf tweaks

--- muckrock/foia/models.py
+++ muckrock/foia/models.py
@@ -655,7 +655,7 @@ class FOIACommunication(models.Model):
     to_who = models.CharField(max_length=255, blank=True)
     priv_from_who = models.CharField(max_length=255, blank=True)
     priv_to_who = models.CharField(max_length=255, blank=True)
-    date = models.DateTimeField()
+    date = models.DateTimeField(db_index=True)
     response = models.BooleanField(help_text='Is this a response (or a request)?')
     full_html = models.BooleanField()
     communication = models.TextField(blank=True)
