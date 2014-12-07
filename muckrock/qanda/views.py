0f66117dd216ac10c0b363d3a2d7dc53950af17f
Mitchell Kotler <mitch@mitchellkotler.net>
2014-12-07 10:41:10 -0500

    add question subscriptions back in

--- muckrock/qanda/views.py
+++ muckrock/qanda/views.py
@@ -133,6 +133,21 @@ def create_answer(request, slug, idx):
         context_instance=RequestContext(request)
     )
 
+@login_required
+def subscribe(request):
+    """Subscribe or unsubscribe to new questions"""
+    profile = request.user.get_profile()
+
+    if profile.follow_questions:
+        profile.follow_questions = False
+        messages.info(request, 'You are now unsubscribed from new question notifications')
+    else:
+        profile.follow_questions = True
+        messages.success(request, 'You are now subscribed to new question notifications')
+    profile.save()
+
+    return redirect(reverse('question-index'))
+
 class List(ListView):
     """List of unanswered questions"""
     paginate_by = 10
