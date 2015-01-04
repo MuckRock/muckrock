d17a071d6e1b5b08a8ed0cb31052756cf18c60ae
Mitchell Kotler <mitch@mitchellkotler.net>
2015-01-04 10:52:39 -0500

    #241 speed up user pages

--- muckrock/accounts/admin.py
+++ muckrock/accounts/admin.py
@@ -2,13 +2,16 @@
 Admin registration for accounts models
 """
 
+from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
+import autocomplete_light
 from reversion import VersionAdmin
 
 from muckrock.accounts.models import Profile, Statistics
+from muckrock.jurisdiction.models import Jurisdiction
 
 # These inhereit more than the allowed number of public methods
 # pylint: disable=R0904
@@ -20,11 +23,23 @@ class StatisticsAdmin(VersionAdmin):
     formats = ['xls', 'csv']
 
 
+class ProfileAdminForm(forms.ModelForm):
+    """Form to include custom choice fields"""
+
+    location = autocomplete_light.ModelChoiceField('JurisdictionAdminAutocomplete',
+                                                   queryset=Jurisdiction.objects.all())
+
+    class Meta:
+        # pylint: disable=R0903
+        model = Profile
+
+
 class ProfileInline(admin.StackedInline):
     """Profile admin options"""
     search_fields = ('user__username', 'user__first_name', 'user__last_name')
     exclude = ('follows_foia', 'follows_question', 'notifications')
     model = Profile
+    form = ProfileAdminForm
     extra = 0
     max_num = 1
 
