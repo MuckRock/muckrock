41ea1e8cdb2e5b513a83c0c5ca3ae3ebc4bfe691
Allan Lasser <lasser.allan@gmail.com>
2015-01-16 17:38:28 -0500

    improves interactions with username search field; excludes added users from search to fix #257

--- muckrock/organization/autocomplete_light_registry.py
+++ muckrock/organization/autocomplete_light_registry.py
@@ -19,17 +19,24 @@ class UserAutocomplete(autocomplete_light.AutocompleteModelBase):
 class OrganizationAutocomplete(UserAutocomplete):
     """Adds organization-specific filtering for users"""
     def choices_for_request(self):
+        # get filters
         query = self.request.GET.get('q', '')
+        exclude = self.request.GET.getlist('exclude', '')
+        org_id = self.request.GET.get('orgId', '')
+        # get all choices
         choices = self.choices.all()
+        # exclude choices based on filters
         if query:
             choices = choices.filter(username__icontains=query)
-        org_id = self.request.GET.get('orgId', '')
+        for user_id in exclude:
+            choices = choices.exclude(pk=int(user_id))
         if org_id: # exclude owner and members from choices
             organization = get_object_or_404(Organization, pk=org_id)
             owner = organization.owner
             profiles = organization.get_members()
             exclude_pks = [owner.pk] + [profile.user.pk for profile in profiles]
             choices = choices.exclude(pk__in=exclude_pks)
+        # return final list of choices
         return self.order_choices(choices)[0:self.limit_choices]
 
 autocomplete_light.register(User, UserAutocomplete)
