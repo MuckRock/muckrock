1907eb2d45d8f876a02ca497ce57898323e1fab1
Allan Lasser <lasser.allan@gmail.com>
2015-01-16 19:23:33 -0500

    catches and handles error when no profile for sidebar messages

--- muckrock/sidebar/context_processors.py
+++ muckrock/sidebar/context_processors.py
@@ -1,6 +1,7 @@
 """
 Context processors to ensure data is displayed in sidebar for all views
 """
+# from django.core.exceptions import DoesNotExist
 
 from muckrock.foia.models import FOIARequest
 from muckrock.sidebar.models import Sidebar
@@ -25,6 +26,9 @@ def sidebar_user_info(request):
 def sidebar_message(request):
     """Displays a message to a given usertype"""
     user = request.user
-    user_class = user.get_profile().acct_type if user.is_authenticated() else 'anonymous'
+    try:
+        user_class = user.get_profile().acct_type if user.is_authenticated() else 'anonymous'
+    except Exception:
+        user_class = 'anonymous'
     message = Sidebar.objects.get_text(user_class)
     return {'broadcast': message}
