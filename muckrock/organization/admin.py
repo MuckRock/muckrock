8fac027a2a5f2148ee7583f5d1c0434f46c7e316
Allan Lasser <lasser.allan@gmail.com>
2015-01-30 17:47:09 -0500

    adds user autoselect to organizations, fixes #273

--- muckrock/organization/admin.py
+++ muckrock/organization/admin.py
@@ -2,20 +2,33 @@
 Admin registration for organization models
 """
 
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 from reversion import VersionAdmin
+import autocomplete_light
 
 from muckrock.organization.models import Organization
 
-# These inhereit more than the allowed number of public methods
-# pylint: disable=R0904
+class OrganizationAdminForm(forms.ModelForm):
+    """Agency admin form to order users"""
+    owner = autocomplete_light.ModelChoiceField(
+        'UserAdminAutocomplete',
+        queryset=User.objects.all(),
+        required=False
+    )
+    
+    class Meta:
+        # pylint: disable=R0903
+        model = Organization
 
 class OrganizationAdmin(VersionAdmin):
-    """Quesiton Admin"""
+    """Organization Admin"""
     prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'owner')
     search_fields = ('name', 'owner')
+    form = OrganizationAdminForm
 
 admin.site.register(Organization, OrganizationAdmin)
 
