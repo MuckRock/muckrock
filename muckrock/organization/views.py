88af2292ea88c14b56b9c4d129b3995dd87f4e5a
Allan Lasser <lasser.allan@gmail.com>
2015-02-04 17:30:52 -0700

    handles some stripe exceptions when creating org subscriptions

--- muckrock/organization/views.py
+++ muckrock/organization/views.py
@@ -16,6 +16,7 @@ from muckrock.organization.forms import OrganizationForm, OrganizationUpdateForm
 from muckrock.settings import STRIPE_PUB_KEY
 
 from datetime import datetime
+import stripe
 
 class List(ListView):
     """List of organizations"""
@@ -121,7 +122,10 @@ def create_organization(request):
             organization.num_requests = organization.monthly_requests
             organization.save()
             organization.create_plan()
-            organization.start_subscription()
+            try:
+                organization.start_subscription()
+            except (stripe.InvalidRequestError, stripe.CardError, ValueError) as exception:
+                messages.error(request, exception)
             profile.organization = organization
             profile.save()
             messages.success(request, 'Your organization has been created.')
@@ -155,7 +159,11 @@ def delete_organization(request, **kwargs):
             member.organization = None
             member.save()
         organization.pause_subscription()
-        organization.delete_plan()
+        try:
+            organization.delete_plan()
+        except ValueError as exception:
+            messages.error(request, exception)
+            return redirect(organization)
         organization.delete()
         messages.success(request, 'Your organization was deleted.')
     elif request.user.get_profile().is_member_of(organization):
@@ -174,7 +182,11 @@ def update_organization(request, **kwargs):
         if form.is_valid():
             organization = form.save()
             if old_cost != organization.monthly_cost:
-                organization.update_plan()
+                try:
+                    organization.update_plan()
+                except (stripe.InvalidRequestError, stripe.CardError, ValueError) as exception:
+                    messages.error(request, exception)
+                    return redirect(organization)
             messages.success(request, 'The organization was updated.')
             return redirect(organization)
     else:
