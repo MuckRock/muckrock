bf5599a88153a2b708f815795f885650debb8905
Mitchell Kotler <mitch@mitchellkotler.net>
2015-02-15 16:22:56 -0500

    rejected template and read only fields on amdin

--- muckrock/task/admin.py
+++ muckrock/task/admin.py
@@ -2,15 +2,45 @@
 Admin registration for Tasks
 """
 
+from reversion import VersionAdmin
+
 from django.contrib import admin
 
 from muckrock.task.models import OrphanTask, SnailMailTask, RejectedEmailTask, \
                                  StaleAgencyTask, FlaggedTask, NewAgencyTask, ResponseTask
 
-admin.site.register(OrphanTask)
-admin.site.register(SnailMailTask)
-admin.site.register(RejectedEmailTask)
-admin.site.register(StaleAgencyTask)
-admin.site.register(FlaggedTask)
-admin.site.register(NewAgencyTask)
-admin.site.register(ResponseTask)
+class OrphanTaskAdmin(VersionAdmin):
+    """Orphan Task Admin"""
+    readonly_fields = ['communication']
+
+class SnailMailTaskAdmin(VersionAdmin):
+    """Snail Mail Task Admin"""
+    readonly_fields = ['communication']
+
+class RejectedEmailTaskAdmin(VersionAdmin):
+    """Rejected Email Task Admin"""
+    readonly_fields = ['foia']
+
+class StaleAgencyTaskAdmin(VersionAdmin):
+    """Stale Agency Task Admin"""
+    readonly_fields = ['agency']
+
+class FlaggedTaskAdmin(VersionAdmin):
+    """Flagged Task Admin"""
+    readonly_fields = ['user', 'foia', 'jurisdiction', 'agency']
+
+class NewAgencyTaskAdmin(VersionAdmin):
+    """New Agency Task Admin"""
+    readonly_fields = ['user', 'agency']
+
+class RepsponseTaskAdmin(VersionAdmin):
+    """Response Task Admin"""
+    readonly_fields = ['communication']
+
+admin.site.register(OrphanTask, OrphanTaskAdmin)
+admin.site.register(SnailMailTask, SnailMailTaskAdmin)
+admin.site.register(RejectedEmailTask, RejectedEmailTaskAdmin)
+admin.site.register(StaleAgencyTask, StaleAgencyTaskAdmin)
+admin.site.register(FlaggedTask, FlaggedTaskAdmin)
+admin.site.register(NewAgencyTask, NewAgencyTaskAdmin)
+admin.site.register(ResponseTask, ResponseTaskAdmin)
