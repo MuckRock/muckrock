e4362be474552fd930622d53b85330ba43bfcda4
Allan Lasser <lasser.allan@gmail.com>
2015-03-13 16:43:19 -0400

    verifies model forms are instanced correctly

--- muckrock/crowdfund/tests.py
+++ muckrock/crowdfund/tests.py
@@ -8,10 +8,15 @@ from mock import Mock
 import nose.tools as _assert
 from datetime import datetime, timedelta
 
-from muckrock.crowdfund import models, forms
+from muckrock.crowdfund import forms
+from muckrock.crowdfund.models import CrowdfundRequest
 from muckrock.foia.models import FOIARequest
 
 class TestCrowdfundRequestForm(TestCase):
+
+    fixtures = ['holidays.json', 'jurisdictions.json', 'agency_types.json', 'test_users.json',
+                'test_agencies.json', 'test_profiles.json', 'test_foiarequests.json',
+                'test_foiacommunications.json']
 
     def setUp(self):
         pass
@@ -20,3 +25,9 @@ class TestCrowdfundRequestForm(TestCase):
         form = forms.CrowdfundRequestForm()
         _assert.ok_(form)
 
+    def test_prefilled_request_form(self):
+        foia = FOIARequest.objects.get(pk=1)
+        due = datetime.now() + timedelta(30)
+        crowdfund = CrowdfundRequest.objects.create(foia=foia, date_due=due)
+        form = forms.CrowdfundRequestForm(instance=crowdfund)
+        _assert.ok_(form)
