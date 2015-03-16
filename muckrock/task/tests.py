89d71cf0c48a07b0187e083ae2fc7d0cea163aa2
Allan Lasser <lasser.allan@gmail.com>
2015-03-16 11:47:00 -0400

    scaffolds list view test class

--- muckrock/task/tests.py
+++ muckrock/task/tests.py
@@ -2,3 +2,16 @@
 Tests for Tasks app
 """
 
+from django.test import TestCase
+
+from mock import Mock, patch
+import nose.tools as nose
+
+class TaskListViewTests(TestCase):
+    """Test that the task list view resolves and renders correctly."""
+
+    def setUp(self):
+        pass
+
+    def test_task_list_url(self):
+        nose.ok_(True)
