09228bb6981684ddc3a5582b78dc31e53bb06942
Allan Lasser <lasser.allan@gmail.com>
2015-03-16 12:05:26 -0400

    ensures login required to access task list

--- muckrock/task/tests.py
+++ muckrock/task/tests.py
@@ -2,7 +2,8 @@
 Tests for Tasks app
 """
 
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
 from mock import Mock, patch
@@ -11,9 +12,17 @@ import nose.tools as nose
 class TaskListViewTests(TestCase):
     """Test that the task list view resolves and renders correctly."""
 
+    fixtures = ['test_users.json']
+
     def setUp(self):
         self.url = reverse('task-list')
+        self.client = Client()
 
     def test_url(self):
         nose.eq_(self.url, '/task/',
             'The task list should be the base task URL')
+
+    def test_login_required(self):
+        response = self.client.get(self.url, follow=True)
+        print(response.status_code)
+        self.assertRedirects(response, '/accounts/login/?next=%s' % self.url)
