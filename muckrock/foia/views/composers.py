4778d9adcb4a11b8755e3ca59b74a139eb41c967
Allan Lasser <lasser.allan@gmail.com>
2015-04-09 18:47:02 -0400

    gets agency filtering right

--- muckrock/foia/views/composers.py
+++ muckrock/foia/views/composers.py
@@ -330,14 +330,23 @@ def create_multirequest(request):
         agency_queries = request.GET.get('query', '').split(' ')
         agencies = {}
         matching_agencies = []
+        # 1. get queryset
+        # 2. transform into listM
+        # 3. transform into set
+        # 4. listN.append(set(listM))
+        # 5. listN = list(reduce(set.intersection, listN))
         for agency_query in agency_queries:
             if len(agency_query) > 2:
-                matching_agencies += list(Agency.objects.filter(approved=True).filter(
+                matching_agencies.append(set(list(Agency.objects.filter(approved=True).filter(
                     Q(name__icontains=agency_query)|
                     Q(aliases__icontains=agency_query)|
                     Q(jurisdiction__name__icontains=agency_query)|
                     Q(types__name__exact=agency_query)
-                ))
+                ))))
+        try:
+            matching_agencies = list(reduce(set.intersection, matching_agencies))
+        except TypeError:
+            matching_agencies = []
         for agency in matching_agencies:
             agencies[agency.name] = agency.id
         return HttpResponse(json.dumps(agencies), content_type='application/json')
