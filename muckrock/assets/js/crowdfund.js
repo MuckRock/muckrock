d2a5ce1e3886a1f221eb3d5a5ca479ccb4f27664
Allan Lasser <lasser.allan@gmail.com>
2015-04-21 21:04:10 -0400

    adds stripe logic into crowdfund logic

--- muckrock/assets/js/crowdfund.js
+++ muckrock/assets/js/crowdfund.js
@@ -16,30 +16,27 @@ function prettifyAmountInput(input) {
     $(input).before(prettyInputElement);
     $(input).attr('hidden', true).hide();
     $(prettyInput).autoNumeric('init', {aSign:'$', pSign:'p'});
-    $(prettyInput).keyup(function(){
-        toggleCrowdfundButton(prettyInput);
-    });
     $(prettyInput).change(function(){
         formatMonies(prettyInput);
     });
-}
-
-function toggleCrowdfundButton(input) {
-    var value = $(input).autoNumeric('get');
-    var disabled = $(button).attr('disabled');
-    var enable;
-    if (value > 0) {
-        $(button).attr('disabled', false);
-        enable = true;
-    } else {
-        $(button).attr('disabled', true);
-        enable = false;
-    }
-    if (disabled && enable) {
-        $(button).removeClass('disabled').addClass('success');
-    } else if (!disabled && !enable) {
-        $(button).removeClass('success').addClass('disabled');
-    }
+    $(button).click(function(e){
+        e.preventDefault();
+        if ($(prettyInput).val) {
+            var checkoutData = getCheckoutData(button);
+            checkout(
+                "{{ stripe_pk }}",
+                "/static/apple-touch-icon.png",
+                checkoutData.description,
+                checkoutData.amount,
+                checkoutData.email,
+                checkoutData.label,
+                checkoutData.form
+            );
+            return false;
+        } else {
+            return false;
+        }
+    });
 }
 
 function formatMonies(input) {
@@ -54,5 +51,52 @@ function formatMonies(input) {
     } else {
         $(button).attr('disabled', true);
         $(button).removeClass('success').addClass('disabled');
+    }
+}
+
+/* Stripe (should figure out how to divorce this from the file */
+
+$('.checkout-button').click(function(e){
+    e.preventDefault();
+
+});
+
+function checkout(pk, image, description, amount, email, label, form, submit) {
+    submit = typeof submit !== 'undefined' ? submit : true;
+    var token = function(token) {
+        form.append('<input type="hidden" name="stripe_token" value="' + token.id + '" />');
+        form.append('<input type="hidden" name="stripe_email" value="' + token.email + '" />');
+        $('a').click(function() { return false; });
+        $('button').click(function() { return false; });
+        if (submit) {
+            form.submit();
+        }
+    }
+    StripeCheckout.open({
+        key: pk,
+        image: image,
+        name: 'MuckRock',
+        description: description,
+        amount: amount,
+        email: email,
+        panelLabel: label,
+        token: token,
+        bitcoin: true
+    });
+}
+
+function getCheckoutData(button) {
+    button = $(button);
+    var amount = button.data('amount');
+    var description = button.data('description');
+    var email = button.data('email');
+    var form = button.data('form');
+    var label = button.data('label');
+    return {
+        'amount': amount,
+        'description': description,
+        'email': email,
+        'label': label,
+        'form': $(form)
     }
 }
