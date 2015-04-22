a8d8cc915eec397b27eddac42aeb86731bc55aa3
Allan Lasser <lasser.allan@gmail.com>
2015-04-22 11:53:25 -0400

    crowdfund payment form logic

--- muckrock/assets/js/crowdfund.js
+++ muckrock/assets/js/crowdfund.js
@@ -1,102 +1,67 @@
 //
 // Provides interface logic for the crowdfunding payment form
+// Depends on Stripe Checkout
 //
 
 var form = '.crowdfund-form';
 var amount = 'input[name=amount]';
-var button = '.crowdfund-form .checkout-button';
+var button = '.crowdfund-form #crowdfund-button';
+var prettyInput = 'input[name=pretty-input]';
 
-prettifyAmountInput(amount);
-
-function prettifyAmountInput(input) {
-    // pretty_amount_input is used as a functional wrapper for the amount input field
-    // progressive enhancement ftw!
-    var prettyInputElement = '<input name="pretty-amount-input" placeholder="$5.00" class="success" id="pretty-amount-input">';
-    var prettyInput = 'input[name=pretty-amount-input]';
-    $(input).before(prettyInputElement);
-    $(input).attr('hidden', true).hide();
-    $(prettyInput).autoNumeric('init', {aSign:'$', pSign:'p'});
-    $(prettyInput).change(function(){
-        formatMonies(prettyInput);
-    });
-    $(button).click(function(e){
-        e.preventDefault();
-        if ($(prettyInput).val) {
-            var checkoutData = getCheckoutData(button);
-            checkout(
-                "{{ stripe_pk }}",
-                "/static/apple-touch-icon.png",
-                checkoutData.description,
-                checkoutData.amount,
-                checkoutData.email,
-                checkoutData.label,
-                checkoutData.form
-            );
-            return false;
-        } else {
-            return false;
-        }
-    });
-}
-
-function formatMonies(input) {
-    var value = $(input).autoNumeric('get');
-    value *= 100;
-    var string_val = $(input).val();
-    if (value > 0) {
-        var description = 'Contribute (' + string_val + ')';
-        $(amount).attr('value', value);
-        $(button).attr('data-amount', value);
-        $(button).attr('data-description', description);
-    } else {
-        $(button).attr('disabled', true);
-        $(button).removeClass('success').addClass('disabled');
+// Stripe (should figure out how to divorce this from the file
+function checkoutCrowdfund(event) {
+    event.preventDefault();
+    var a = $(amount).val();
+    if (!a) {
+        return false;
     }
-}
 
-/* Stripe (should figure out how to divorce this from the file */
-
-$('.checkout-button').click(function(e){
-    e.preventDefault();
-
-});
-
-function checkout(pk, image, description, amount, email, label, form, submit) {
-    submit = typeof submit !== 'undefined' ? submit : true;
+    var b = $(button);
+    var f = $(form);
+    var key = b.data('key');
+    var icon = b.data('icon');
+    var email = b.data('email');
+    var label = 'Contribute';
+    var description = 'Contribute (' + $(prettyInput).val() + ')';
     var token = function(token) {
-        form.append('<input type="hidden" name="stripe_token" value="' + token.id + '" />');
-        form.append('<input type="hidden" name="stripe_email" value="' + token.email + '" />');
         $('a').click(function() { return false; });
         $('button').click(function() { return false; });
-        if (submit) {
-            form.submit();
-        }
+        f.append('<input type="hidden" name="token" value="' + token.id + '" />');
+        f.append('<input type="hidden" name="email" value="' + token.email + '" />');
+        f.submit();
     }
     StripeCheckout.open({
-        key: pk,
-        image: image,
+        key: key,
+        image: icon,
         name: 'MuckRock',
         description: description,
-        amount: amount,
+        amount: a,
+        bitcoin: true,
         email: email,
         panelLabel: label,
-        token: token,
-        bitcoin: true
+        token: token
+    });
+
+    return true;
+}
+
+function prettifyAmountInput(input, pretty) {
+    // pretty_amount_input is used as a functional wrapper for the amount input field
+    // progressive enhancement ftw!
+    var prettyInputElement = '<input name="pretty-input" placeholder="$5.00" class="success" >';
+    $(input).before(prettyInputElement);
+    $(input).attr('hidden', true).hide();
+    $(pretty).autoNumeric('init', {aSign:'$', pSign:'p'});
+    function inputWrapper(input) {
+        var value = $(input).autoNumeric('get') * 100;
+        $(amount).attr('value', value);
+    }
+    $(pretty).change(function(e){
+        inputWrapper(this);
     });
 }
 
-function getCheckoutData(button) {
-    button = $(button);
-    var amount = button.data('amount');
-    var description = button.data('description');
-    var email = button.data('email');
-    var form = button.data('form');
-    var label = button.data('label');
-    return {
-        'amount': amount,
-        'description': description,
-        'email': email,
-        'label': label,
-        'form': $(form)
-    }
-}
+prettifyAmountInput(amount, prettyInput);
+$(button).click(function(e){
+    checkoutCrowdfund(e);
+});
