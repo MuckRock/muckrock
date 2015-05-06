e1e33afc7c40705be4b343d8942a48002276e4b7
Allan Lasser <lasser.allan@gmail.com>
2015-05-06 15:24:45 -0400

    removes the num_range tag, turns out i do not need it!

--- muckrock/templatetags/templatetags/tags.py
+++ muckrock/templatetags/templatetags/tags.py
@@ -37,45 +37,6 @@ def page_link(request, page_num):
             href += '&%s=%s' % (key, escape(value))
     return href
 
-# num_range tag via https://djangosnippets.org/snippets/779/
-
-class RangeNode(Node):
-    def __init__(self, num, context_name):
-        self.num, self.context_name = num, context_name
-    def render(self, context):
-        context[self.context_name] = range(int(self.num))
-        return ""
-
-@register.tag
-def num_range(parser, token):
-    """
-    Takes a number and iterates and returns a range (list) that can be
-    iterated through in templates
-
-    Syntax:
-    {% num_range 5 as some_range %}
-
-    {% for i in some_range %}
-      {{ i }}: Something I want to repeat\n
-    {% endfor %}
-
-    Produces:
-    0: Something I want to repeat
-    1: Something I want to repeat
-    2: Something I want to repeat
-    3: Something I want to repeat
-    4: Something I want to repeat
-    """
-    try:
-        fnctn, num, trash, context_name = token.split_contents()
-    except ValueError:
-        raise TemplateSyntaxError, "%s takes the syntax %s number_to_iterate\
-            as context_variable" % (fnctn, fnctn)
-    if not trash == 'as':
-        raise TemplateSyntaxError, "%s takes the syntax %s number_to_iterate\
-            as context_variable" % (fnctn, fnctn)
-    return RangeNode(num, context_name)
-
 @register.filter
 @stringfilter
 def company_title(companies):
