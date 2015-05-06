2be7a4c4410d5ef89e1dc745dfdd422cbd0dc73e
Allan Lasser <lasser.allan@gmail.com>
2015-05-06 19:32:17 -0400

    gets a functional template tag up and running

--- muckrock/task/templatetags/task_tags.py
+++ muckrock/task/templatetags/task_tags.py
@@ -2,6 +2,8 @@ from django import template
 
 from muckrock.task.models import Task
 
+import logging
+
 register = template.Library()
 
 class TaskNode(template.Node):
@@ -11,30 +13,30 @@ class TaskNode(template.Node):
 
     def __init__(self, task_id):
         """The node should be initialized with a task object"""
-        self.task = self.model.objects.get(id=task_id)
+        self.task_id = template.Variable(task_id)
 
     def render(self, context):
-        """Render the table headers"""
-        context.update(self.get_extra_context())
+        """Render the task"""
+        try:
+            task = self.model.objects.get(id=self.task_id.resolve(context))
+        except (template.VariableDoesNotExist, self.model.DoesNotExist):
+            return ''
+        context.update(self.get_extra_context(task))
         return template.loader.render_to_string(
-            task_template,
-            context,
-            context_instance=template.RequestContext(self.request)
+            self.task_template,
+            context
         )
 
-    def get_extra_context(self):
+    def get_extra_context(self, task):
         """Returns a dictionary of context for the specific task"""
-        extra_context = {'task': self.task}
+        extra_context = {'task': task}
         return extra_context
 
 @register.tag
 def task(parser, token):
     """Returns the correct task node given a task ID"""
     try:
-        task_id = token[1:]
-        return TaskNode(task_id)
-    except IndexError:
-        logging.error('No argument provided to task tag.')
-    except Task.DoesNotExist:
-        logging.error('The task does not exist.')
-    return None
+        tag_name, task_id = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError("%s tag requires a single argument." % token.contents.split()[0])
+    return TaskNode(task_id)
