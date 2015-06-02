4d6582df31f48d69324116689fb3c151f0895e3a
Mitchell Kotler <mitch@mitchellkotler.net>
2015-06-01 21:16:56 -0400

    close #420, stop storing google analytics data

--- muckrock/accounts/tasks.py
+++ muckrock/accounts/tasks.py
@@ -8,7 +8,6 @@ from celery.task import periodic_task
 from django.contrib.auth.models import User
 from django.db.models import Sum
 
-import gdata.analytics.service
 import logging
 from datetime import date, timedelta
 
@@ -16,7 +15,6 @@ from muckrock.accounts.models import Profile, Statistics
 from muckrock.agency.models import Agency
 from muckrock.foia.models import FOIARequest, FOIAFile, FOIACommunication
 from muckrock.news.models import Article
-from muckrock.settings import GA_USERNAME, GA_PASSWORD, GA_ID
 from muckrock.task.models import Task, OrphanTask, SnailMailTask, RejectedEmailTask, \
                                  StaleAgencyTask, FlaggedTask, NewAgencyTask, ResponseTask, \
                                  GenericTask, PaymentTask, CrowdfundTask, FailedFaxTask
@@ -28,13 +26,6 @@ def store_statstics():
     """Store the daily statistics"""
 
     yesterday = date.today() - timedelta(1)
-
-    client = gdata.analytics.service.AnalyticsDataService()
-    client.ssl = True
-    client.ClientLogin(GA_USERNAME, GA_PASSWORD)
-    data = client.GetData(ids=GA_ID, metrics='ga:pageviews', start_date=yesterday.isoformat(),
-                          end_date=yesterday.isoformat())
-    total_page_views = data.entry[0].pageviews.value
 
     stats = Statistics.objects.create(
         date=yesterday,
@@ -57,7 +48,6 @@ def store_statstics():
         total_fees=FOIARequest.objects.aggregate(Sum('price'))['price__sum'],
         pro_users=Profile.objects.filter(acct_type='pro').count(),
         pro_user_names=';'.join(p.user.username for p in Profile.objects.filter(acct_type='pro')),
-        total_page_views=total_page_views,
         daily_requests_pro=FOIARequest.objects.filter(
             user__profile__acct_type='pro',
             date_submitted=yesterday
