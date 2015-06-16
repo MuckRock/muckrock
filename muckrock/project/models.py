0ead153a96873dfcceaa528f69ad82c74dccf125
Allan Lasser <lasser.allan@gmail.com>
2015-06-16 18:32:27 -0400

    fixes unicode method to actually return unicode :)

--- muckrock/project/models.py
+++ muckrock/project/models.py
@@ -1,3 +1,7 @@
+"""
+Models for the project application.
+"""
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -6,6 +10,7 @@ from muckrock.tags.models import TaggedItemBase
 from taggit.managers import TaggableManager
 
 class Project(models.Model):
+    """Projects are a mixture of general and specific information on a broad subject."""
     title = models.CharField(max_length=100, help_text='Titles are limited to 100 characters.')
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='project_images', blank=True, null=True)
@@ -28,7 +33,7 @@ class Project(models.Model):
     tags = TaggableManager(through=TaggedItemBase, blank=True)
 
     def __unicode__(self):
-        return self.title
+        return unicode(self.title)
 
     def make_private(self):
         """Sets a project to be private."""
