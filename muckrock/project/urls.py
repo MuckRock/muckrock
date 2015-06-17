d7508eeebceb0bbc153af2e889fd4372d5c42175
Allan Lasser <lasser.allan@gmail.com>
2015-06-17 16:13:11 -0400

    requires login for creating project

--- muckrock/project/urls.py
+++ muckrock/project/urls.py
@@ -3,9 +3,10 @@ URL routes for the project application
 """
 
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from muckrock.project import views
 
 urlpatterns = patterns('',
-    url(r'^create/$', views.CreateProjectView.as_view(), name='project-create'),
+    url(r'^create/$', login_required(views.CreateProjectView.as_view()), name='project-create'),
 )
