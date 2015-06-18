ac64402811534b9938b2893f8506d4aaa79a5122
Allan Lasser <lasser.allan@gmail.com>
2015-06-18 12:53:52 -0400

    adds delete project view; passes delete project test

--- muckrock/project/views.py
+++ muckrock/project/views.py
@@ -2,13 +2,14 @@
 Views for the project application
 """
 
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from muckrock.project.models import Project
 from muckrock.project.forms import ProjectCreateForm, ProjectUpdateForm
 
 class ProjectCreateView(CreateView):
-    """Create a project instance."""
+    """Create a project instance"""
     form_class = ProjectCreateForm
     template_name = 'project/create.html'
 
@@ -22,3 +23,9 @@ class ProjectUpdateView(UpdateView):
     model = Project
     form_class = ProjectUpdateForm
     template_name = 'project/update.html'
+
+class ProjectDeleteView(DeleteView):
+    """Delete a project instance"""
+    model = Project
+    success_url = reverse_lazy('index')
+    template_name = 'project/delete.html'
