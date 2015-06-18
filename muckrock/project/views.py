4e2f28d7e0f6df89e3c6de1c0182fbd8407b4448
Allan Lasser <lasser.allan@gmail.com>
2015-06-18 11:11:08 -0400

    refactors creation view

--- muckrock/project/views.py
+++ muckrock/project/views.py
@@ -3,31 +3,15 @@ Views for the project application
 """
 
 from django.shortcuts import render, redirect
-from django.views.generic import View, DetailView
-from django.utils.text import slugify
+from django.views.generic import CreateView, DetailView
 
 from muckrock.project.models import Project
 from muckrock.project.forms import CreateProjectForm
 
-class CreateProjectView(View):
+class CreateProjectView(CreateView):
     """View for creating a new project."""
     form_class = CreateProjectForm
     template_name = 'project/create.html'
-
-    def get(self, request):
-        """Renders a template with a form for creating the project"""
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        """Creates a project based on the received data"""
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            project = form.save(commit=False)
-            project.slug = slugify(project.title)
-            project.save()
-            return redirect(project)
-        return render(request, self.template_name, {'form': form})
 
 class ProjectDetailView(DetailView):
     """Detail about a specific project"""
