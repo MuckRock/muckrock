d207bd555afeed54dad8611567ba7068df03179c
Allan Lasser <lasser.allan@gmail.com>
2015-06-18 17:01:57 -0400

    passes test for requiring login to update project

--- muckrock/project/urls.py
+++ muckrock/project/urls.py
@@ -8,8 +8,14 @@ from django.contrib.auth.decorators import login_required
 from muckrock.project import views
 
 urlpatterns = patterns('',
-    url(r'^create/$', login_required(views.ProjectCreateView.as_view()), name='project-create'),
-    url(r'^(?P<slug>[\w-]+)/$', views.ProjectDetailView.as_view(), name='project-detail'),
-    url(r'^(?P<slug>[\w-]+)/update/$', views.ProjectUpdateView.as_view(), name='project-update'),
+    url(r'^create/$',
+        login_required(views.ProjectCreateView.as_view()),
+        name='project-create'),
+    url(r'^(?P<slug>[\w-]+)/$',
+        views.ProjectDetailView.as_view(),
+        name='project-detail'),
+    url(r'^(?P<slug>[\w-]+)/update/$',
+        login_required(views.ProjectUpdateView.as_view()),
+        name='project-update'),
     url(r'^(?P<slug>[\w-]+)/delete/$', views.ProjectDeleteView.as_view(), name='project-delete')
 )
