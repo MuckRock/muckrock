82abf5819e94868ea78ff1d48115e92657ccab84
Mitchell Kotler <mitch@mitchellkotler.net>
2015-06-24 19:13:00 -0400

    remove livesettings config

--- muckrock/foia/models/__init__.py
+++ muckrock/foia/models/__init__.py
@@ -6,6 +6,3 @@ from muckrock.foia.models.request import *
 from muckrock.foia.models.multirequest import *
 from muckrock.foia.models.communication import *
 from muckrock.foia.models.file import *
-
-from muckrock.foia import config
-
