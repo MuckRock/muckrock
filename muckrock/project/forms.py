e0a77fdbda5784e212a565ee33b4505b92953705
Allan Lasser <lasser.allan@gmail.com>
2015-07-08 15:22:28 -0400

    adds autosuggestions to tag field, fixes #470

--- muckrock/project/forms.py
+++ muckrock/project/forms.py
@@ -5,10 +5,17 @@ Forms for the project application
 from django import forms
 
 import autocomplete_light
+from autocomplete_light.contrib.taggit_field import TaggitField, TaggitWidget
+
 from muckrock.project.models import Project
 
 class ProjectCreateForm(forms.ModelForm):
     """Form for creating a new project"""
+
+    tags = TaggitField(
+        widget=TaggitWidget('TagAutocomplete', attrs={'placeholder': 'Search tags'}),
+        help_text='Separate tags with commas.'
+    )
 
     class Meta:
         model = Project
@@ -21,6 +28,11 @@ class ProjectCreateForm(forms.ModelForm):
 
 class ProjectUpdateForm(forms.ModelForm):
     """Form for updating a project instance"""
+
+    tags = TaggitField(
+        widget=TaggitWidget('TagAutocomplete', attrs={'placeholder': 'Search tags'}),
+        help_text='Separate tags with commas.'
+    )
 
     class Meta:
         model = Project
