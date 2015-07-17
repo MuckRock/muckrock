b79f37ae4fd410c16be02e745691bd57aa4c8b0d
Allan Lasser <lasser.allan@gmail.com>
2015-07-17 15:40:58 -0400

    refactors url tests into separate file

---
+++ muckrock/organization/tests/test_urls.py
@@ -0,0 +1,41 @@
+"""
+Test organization urls
+"""
+
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+
+from muckrock.organization.models import Organization
+
+from datetime import datetime
+from mock import Mock, patch
+import nose.tools.eq_ as eq_
+
+class OrganizationURLTests(TestCase):
+    """Test the urls for the organization app"""
+
+    def setUp(self):
+        """Set up models for the organization"""
+        owner = Mock(spec=User)
+        org = Mock(spec=Organization)
+        self.client = Client()
+
+    def test_index(self):
+        """The index should be OK"""
+        response = self.client.get('/organization/')
+        eq_(response.status_code, 200)
+
+    def test_create(self):
+        """Create should redirect"""
+        response = self.client.get('/organization/create/')
+        eq_(response.status_code, 302)
+
+    def test_detail(self):
+        """Detail page should be OK"""
+        response = self.client.get('/organization/test-organization/')
+        eq_(response.status_code, 200)
+
+    def test_delete(self):
+        """ordinary users should not be able to access the delete page, hence 404"""
+        response = self.client.get('/organization/test-orgainzation/delete/')
+        eq_(response.status_code, 404)
--- muckrock/organization/tests/tests.py
+++ muckrock/organization/tests/tests.py
@@ -16,42 +16,6 @@ import stripe
 ok_ = nose.tools.ok_
 eq_ = nose.tools.eq_
 
-class OrganizationURLTests(TestCase):
-    """Test the views for the organization app"""
-
-    def setUp(self):
-        """Set up models for the organization"""
-        owner = User.objects.create(
-            username='TestOwner',
-            password='testowner'
-        )
-        Organization.objects.create(
-            name='Test Organization',
-            slug='test-organization',
-            owner=owner,
-            date_update=datetime.now(),
-        )
-
-    def test_index(self):
-        """The index should be OK"""
-        response = self.client.get('/organization/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_create(self):
-        """Create should redirect"""
-        response = self.client.get('/organization/create/')
-        self.assertEqual(response.status_code, 302)
-
-    def test_detail(self):
-        """Detail page should be OK"""
-        response = self.client.get('/organization/test-organization/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_delete(self):
-        """ordinary users should not be able to access the delete page, hence 404"""
-        response = self.client.get('/organization/test-orgainzation/delete/')
-        self.assertEqual(response.status_code, 404)
-
 # Creates mock items for testing methods that involve Stripe
 mock_customer = Mock()
 MockCustomer = Mock()
