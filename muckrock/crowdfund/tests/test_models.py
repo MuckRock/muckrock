e69db80ae33c9269da4344286149828539745ccd
Allan Lasser <lasser.allan@gmail.com>
2015-07-20 14:34:46 -0400

    rewrites crowdfund basic test to pass

--- muckrock/crowdfund/tests/test_models.py
+++ muckrock/crowdfund/tests/test_models.py
@@ -5,20 +5,17 @@ from mock import Mock, patch
 from nose.tools import ok_, eq_
 
 from muckrock.crowdfund import models
+from muckrock.project.models import Project
 
 class TestCrowdfundProject(TestCase):
     """Test crowdfunding a project"""
 
     def setUp(self):
-        self.project = Mock()
-        self.project.title = 'Test Project'
-        due_date = date.today() + timedelta(30)
-        # create a crowdfund
-        self.crowdfund = models.CrowdfundProject(
-            name = 'Cool project please help',
-            due_date = due_date,
-            project = self.project
-        )
+        self.crowdfund = models.CrowdfundProject()
+        self.crowdfund.name = 'Cool project please help'
+        self.crowdfund.due_date = date.today() + timedelta(30)
+        project = Project.objects.create(title='Test Project')
+        self.crowdfund.project = project
 
     def test_unicode(self):
         eq_('%s' % self.crowdfund, 'Crowdfunding for Test Project')
