d486c19268dd23b5d44c40c47e53ed0d6aab8db3
Allan Lasser <lasser.allan@gmail.com>
2015-07-23 09:39:06 -0400

    wraps detail view form use in a try block

--- muckrock/crowdfund/views.py
+++ muckrock/crowdfund/views.py
@@ -114,10 +114,12 @@ class CrowdfundDetailView(DetailView):
             amount = crowdfund_object.amount_remaining()
         payment_data = {'amount': amount, 'show': show, 'crowdfund': crowdfund}
         payment_form = self.get_form()
-        # pylint:disable=not-callable
-        # pylint believes payment form is uncallable because in this view it is None
-        payment_form = payment_form(payment_data)
-        # pylint:enable=not-callable
+        try:
+            payment_form = payment_form(payment_data)
+        except TypeError:
+            logging.error(('The subclassed object does not have a form attribute '
+                           'so no payments can be made.'))
+            raise ValueError('%s does not have its form attribute set.' % self.__class__)
         payment_object = None
         if payment_form.is_valid() and email and token:
             if process_payment(request, amount, token, crowdfund_object):
