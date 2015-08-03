e4607db83022cd83b53fde6bf21f4931b31b7b55
Allan Lasser <lasser.allan@gmail.com>
2015-08-03 11:14:56 -0400

    creates a test file for the tag index

--- muckrock/tags/tests.py
+++ muckrock/tags/tests.py
@@ -1,3 +1,23 @@
 """
 Tests for tags
 """
+from django import test
+from django.core.urlresolvers import reverse
+
+from nose.tools import ok_
+
+class TestTagListView(test.TestCase):
+    """
+    The tag list view should display each tag in a filterable list.
+    For each tag in the list, it should display the MuckRock objects
+    associated with that tag.
+    """
+
+    def setUp(self):
+        self.client = test.Client()
+
+    def test_resolve_url(self):
+        """The tag list url should resolve."""
+        tag_url = reverse('/index')
+        response = self.client.get(tag_url)
+        ok_(response.status_code == 200)
