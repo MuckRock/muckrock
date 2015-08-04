39375b1261e3fdf073269dc0921f38eebe8d2d6a
Allan Lasser <lasser.allan@gmail.com>
2015-08-04 18:48:58 -0400

    fixes apps files for accounts and projects

--- muckrock/project/apps.py
+++ muckrock/project/apps.py
@@ -8,8 +8,8 @@ from actstream import registry
 
 class ProjectConfig(AppConfig):
     """Configures the project application to use activity streams"""
-    name = 'project'
+    name = 'muckrock.project'
 
     def ready(self):
         """Registers the application with the activity streams plugin"""
-        registry.register(self.get_model('Project')
+        registry.register(self.get_model('Project'))
