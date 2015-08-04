9fe79dfac3adb6d3edc76a5dba6bd663df55c391
Allan Lasser <lasser.allan@gmail.com>
2015-08-04 18:48:58 -0400

    fixes apps files for accounts and projects

--- muckrock/accounts/apps.py
+++ muckrock/accounts/apps.py
@@ -3,13 +3,15 @@ App config for accounts
 """
 
 from django.apps import AppConfig
+from django.contrib.auth.models import User
 
 from actstream import registry
 
 class AccountsConfig(AppConfig):
     """Configures the accounts application to use activity streams"""
-    name = 'accounts'
+    name = 'muckrock.accounts'
 
     def ready(self):
         """Registers users with the activity streams plugin"""
-        registry.register(self.get_model('User'))
+        registry.register(User)
+        registry.register(self.get_model('Profile'))
