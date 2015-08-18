737cac4bb5b4c30a4632e48ab718b6653c180d77
Allan Lasser <lasser.allan@gmail.com>
2015-08-18 16:01:58 -0400

    creates a recurring task for closing expired crowdfunds

---
+++ muckrock/crowdfund/tests/test_tasks.py
@@ -0,0 +1,42 @@
+"""
+Test recurring crowdfund tasks
+"""
+
+from django import test
+
+from datetime import date, timedelta
+from mock import Mock
+from nose.tools import ok_
+
+from muckrock.crowdfund import models, tasks
+from muckrock.project.models import Project
+
+class TestRecurringTasks(test.TestCase):
+    """Test recurring crowdfund tasks"""
+    def setUp(self):
+        self.project = Project.objects.create(title='Cool project')
+
+    def test_close_expired(self):
+        crowdfund = models.CrowdfundProject.objects.create(
+            name='Cool project please help',
+            date_due=(date.today() - timedelta(1)),
+            payment_required=20.00,
+            project=self.project
+        )
+        crowdfund.save()
+        tasks.close_expired()
+        updated_crowdfund = models.CrowdfundProject.objects.get(pk=crowdfund.pk)
+        ok_(updated_crowdfund.closed, 'Any crowdfund past its date due should be closed.')
+
+    def test_do_not_close_today(self):
+        crowdfund = models.CrowdfundProject.objects.create(
+            name='Cool project please help',
+            date_due=(date.today()),
+            payment_required=20.00,
+            project=self.project
+        )
+        crowdfund.save()
+        tasks.close_expired()
+        updated_crowdfund = models.CrowdfundProject.objects.get(pk=crowdfund.pk)
+        ok_(not updated_crowdfund.closed,
+            'Crowdfunds with a due date of today should not be closed.')
