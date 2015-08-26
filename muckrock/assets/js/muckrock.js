0bbdce6ad72d2db0f0a01a73830926527dcf445a
Allan Lasser <lasser.allan@gmail.com>
2015-08-26 15:49:46 -0400

    removes unused js code

--- muckrock/assets/js/muckrock.js
+++ muckrock/assets/js/muckrock.js
@@ -113,40 +113,6 @@ $('.message .visibility').click(function() {
     }
 });
 
-if ($.cookie('broadcast') == false) {
-    var broadcastPanel= $('.sidebar .broadcast')[0];
-    $(broadcastPanel).hide();
-}
-$('#close-broadcast').click(function(){
-    $(this).parent().hide();
-    $.cookie('broadcast', 0);
-});
-
-/* Key and Swipe Bindings
-$(document).bind('keydown', 'm', toggleSidebar());
-$(document).bind('keydown', 'shift+m', toggleSidebar());
-$(document).bind('keydown', 'left', toggleSidebar());
-$(document).bind('keydown', 'right', toggleSidebarOff());
-$(document).bind('keydown', 'esc', toggleSidebarOff());
-// swipe left to toggle sidebar on
-// swipe right to toggle sidebar off
-
-// Sidebar Interactions
-//
-// if sidebar is open
-//      on clicking or tapping div.container or div.footer-container:
-//          sidebar closes
-//      on clicking or tapping a.menu-button:
-//          sidebar closes
-//          a.menu-button content changes
-// if sidebar is closed
-//      on clicking or tapping a.menu-button:
-//          sidebar opens
-//          a.menu-button content changes
-*/
-
-
-
 // formsets
 $(function() {
 	$('.formset-container').formset();
