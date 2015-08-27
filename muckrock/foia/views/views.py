8b97a8528c22e16d5771e735cc5a16c5b2c6d676
Allan Lasser <lasser.allan@gmail.com>
2015-08-27 16:06:16 -0400

    makes an actionable estimated date component

--- muckrock/foia/views/views.py
+++ muckrock/foia/views/views.py
@@ -149,12 +149,13 @@ class Detail(DetailView):
         is_past_due = foia.date_due < datetime.now().date() if foia.date_due else False
         context['all_tags'] = Tag.objects.all()
         context['past_due'] = is_past_due
+        context['user_can_edit'] = foia.editable_by(user)
         context['admin_actions'] = foia.admin_actions(user)
         context['user_actions'] = foia.user_actions(user)
         context['noncontextual_request_actions'] = foia.noncontextual_request_actions(user)
         context['contextual_request_actions'] = foia.contextual_request_actions(user)
         context['status_choices'] = STATUS if user.is_staff or foia.status == 'started' else STATUS_NODRAFT
-        context['show_estimated_date'] = foia.status not in ['ack', 'done', 'rejected']
+        context['show_estimated_date'] = foia.status not in ['submitted', 'ack', 'done', 'rejected']
         context['stripe_pk'] = STRIPE_PUB_KEY
         context['sidebar_admin_url'] = reverse('admin:foia_foiarequest_change', args=(foia.pk,))
         if foia.sidebar_html:
