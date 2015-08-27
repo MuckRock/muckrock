cefa81941b172a71b525c1e92da3cd9e0efc21ff
Allan Lasser <lasser.allan@gmail.com>
2015-08-27 19:43:38 -0400

    finishes adding click listeners to communication header

--- muckrock/assets/js/communication.js
+++ muckrock/assets/js/communication.js
@@ -1,3 +1,13 @@
 $('.communication-header').click(function(){
     $(this).closest('.communication').toggleClass('collapsed');
 });
+
+$('.communication-header').find('a').click(function(){
+    window.location.href = $(this).attr('href');
+    return false;
+});
+
+$('.communication .options .svgIcon').click(function(){
+    $(this).siblings('.dropdown').toggleClass('visible');
+    return false;
+});
