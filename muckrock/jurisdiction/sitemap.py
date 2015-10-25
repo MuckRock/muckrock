44ac1d5d67d9786e0b959fd713161994a64e7cd6
Mitchell Kotler <mitch@mitchellkotler.net>
2015-10-25 16:27:06 -0400

    reduce limit on jurisdiction sitemap

--- muckrock/jurisdiction/sitemap.py
+++ muckrock/jurisdiction/sitemap.py
@@ -6,15 +6,12 @@ from django.contrib.sitemaps import Sitemap
 
 from muckrock.jurisdiction.models import Jurisdiction
 
-class LimitSitemap(Sitemap):
-    """Limit Sitemap"""
-    limit = 500
-
-class JurisdictionSitemap(LimitSitemap):
+class JurisdictionSitemap(Sitemap):
     """Sitemap for Jurisdiction Requests"""
 
     priority = 0.7
     changefreq = 'monthly'
+    limit = 200
 
     def items(self):
         """Return all non hidden Jurisdictions"""
