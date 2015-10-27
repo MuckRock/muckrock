3de994d69ff7a3f8d9ccc78dd2dd812e3e0bd254
Allan Lasser <lasser.allan@gmail.com>
2015-10-27 14:18:01 -0400

    writes simple test cases for the daily notification object

--- muckrock/notification/tests.py
+++ muckrock/notification/tests.py
@@ -1,3 +1,27 @@
+"""
+Tests the notification application.
+"""
+
 from django.test import TestCase
 
-# Create your tests here.
+import nose.tools
+
+from muckrock import factories
+from muckrock.notification.models import DailyNotification
+
+ok_ = nose.tools.ok_
+raises = nose.tools.raises
+
+class TestDailyNotification(TestCase):
+    """Tests the daily email notification object."""
+    def setUp(self):
+        self.user = factories.UserFactory()
+
+    def test_init(self):
+        """The email should create when given a User."""
+        ok_(DailyNotification(self.user))
+
+    @raises(TypeError)
+    def test_requires_user(self):
+        """The email should raise an error when instantiated without a user."""
+        DailyNotification()
