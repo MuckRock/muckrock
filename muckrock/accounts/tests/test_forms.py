9f18ea033bd93d204dfeef9c306df5f1a0e1be49
Allan Lasser <lasser.allan@gmail.com>
2016-01-08 17:22:46 -0500

    sends notification when email is changed

--- muckrock/accounts/tests/test_forms.py
+++ muckrock/accounts/tests/test_forms.py
@@ -5,7 +5,8 @@ Tests accounts forms
 from django.test import TestCase
 from django.forms import ValidationError
 
-from nose.tools import eq_, raises, assert_false
+from nose.tools import eq_, raises, assert_false, assert_true
+from mock import patch
 
 from muckrock.accounts.forms import EmailSettingsForm, RegisterForm
 from muckrock.factories import UserFactory, ProfileFactory
@@ -17,23 +18,32 @@ class TestEmailSettingsForm(TestCase):
         self.profile = ProfileFactory()
         self.form = EmailSettingsForm(instance=self.profile)
 
-    def test_email_normal(self):
+    @patch('muckrock.message.tasks.email_change.delay')
+    def test_email_normal(self, mock_notify):
         """Changing email normally should succeed"""
         new_email = 'new@example.com'
-        self.form.cleaned_data = {'email': new_email}
+        self.form = EmailSettingsForm({'email': new_email}, instance=self.profile)
+        assert_true(self.form.is_valid())
         eq_(self.form.clean_email(), new_email)
+        self.form.save()
+        mocK_notify.assert_called_once()
 
-    def test_email_same(self):
+    @patch('muckrock.message.tasks.email_change.delay')
+    def test_email_same(self, mock_notify):
         """Keeping email the same should succeed"""
         existing_email = self.profile.user.email
-        self.form.cleaned_data = {'email': existing_email}
+        self.form.data = {'email': existing_email}
+        assert_true(self.form.is_valid())
         eq_(self.form.clean_email(), existing_email)
+        self.form.save()
+        mock_notify.assert_not_called()
 
     @raises(ValidationError)
     def test_email_conflict(self):
         """Trying to use an already taken email should fail"""
         other_user = UserFactory()
-        self.form.cleaned_data = {'email': other_user.email}
+        self.form.data = {'email': other_user.email}
+        assert_false(self.form.is_valid())
         self.form.clean_email()
 
 
