833f8724ce98f5e57b886019fab9ee34f00f7e96
Allan Lasser <lasser.allan@gmail.com>
2016-01-14 17:34:21 -0500

    fixes tests

--- muckrock/accounts/tests/test_forms.py
+++ muckrock/accounts/tests/test_forms.py
@@ -16,35 +16,39 @@ class TestEmailSettingsForm(TestCase):
     def setUp(self):
         """Set up tests"""
         self.profile = ProfileFactory()
-        self.form = EmailSettingsForm(instance=self.profile)
+        self.data = {
+            'email_pref': self.profile.email_pref,
+            'use_autologin': self.profile.use_autologin,
+            'email': self.profile.user.email
+        }
+        self.form = EmailSettingsForm
 
     @patch('muckrock.message.tasks.email_change.delay')
     def test_email_normal(self, mock_notify):
         """Changing email normally should succeed"""
         new_email = 'new@example.com'
-        self.form = EmailSettingsForm({'email': new_email}, instance=self.profile)
-        assert_true(self.form.is_valid())
-        eq_(self.form.clean_email(), new_email)
-        self.form.save()
-        mocK_notify.assert_called_once()
+        self.data['email'] = new_email
+        form = self.form(self.data, instance=self.profile)
+        assert_true(form.is_valid())
+        eq_(form.clean_email(), new_email)
+        form.save()
+        mock_notify.assert_called_once()
 
     @patch('muckrock.message.tasks.email_change.delay')
     def test_email_same(self, mock_notify):
         """Keeping email the same should succeed"""
-        existing_email = self.profile.user.email
-        self.form.data = {'email': existing_email}
-        assert_true(self.form.is_valid())
-        eq_(self.form.clean_email(), existing_email)
-        self.form.save()
+        form = self.form(self.data, instance=self.profile)
+        assert_true(form.is_valid())
+        eq_(form.clean_email(), self.profile.user.email)
+        form.save()
         mock_notify.assert_not_called()
 
-    @raises(ValidationError)
     def test_email_conflict(self):
         """Trying to use an already taken email should fail"""
         other_user = UserFactory()
-        self.form.data = {'email': other_user.email}
-        assert_false(self.form.is_valid())
-        self.form.clean_email()
+        self.data['email'] = other_user.email
+        form = self.form(self.data, instance=self.profile)
+        assert_false(form.is_valid())
 
 
 class TestRegistrationForm(TestCase):
