27afb351c0e9955839a54fd52f8ddb1691adde9e
Mitchell Kotler <mitch@mitchellkotler.net>
2016-01-16 10:09:03 -0500

    pylint

--- muckrock/middleware.py
+++ muckrock/middleware.py
@@ -4,12 +4,6 @@ Middleware for MuckRock
 
 from django.http import HttpResponseRedirect
 
-import hotshot, hotshot.stats
-import os
-import re
-import StringIO
-import sys
-import tempfile
 from urllib import urlencode
 
 from muckrock import settings
@@ -19,21 +13,10 @@ class RemoveTokenMiddleware(object):
 
     def process_request(self, request):
         """Remove login token from URL"""
+        # pylint: disable=no-self-use
         if settings.LOT_MIDDLEWARE_PARAM_NAME in request.GET:
             params = request.GET.copy()
             params.pop(settings.LOT_MIDDLEWARE_PARAM_NAME)
             return HttpResponseRedirect(
                     '%s?%s' % (request.path, urlencode(params)))
-
-# Orignal version taken from http://www.djangosnippets.org/snippets/186/
-# Original author: udfalkso
-# Modified by: Shwagroo Team and Gun.io
-
-words_re = re.compile( r'\s+' )
-
-group_prefix_re = [
-    re.compile( "^.*/django/[^/]+" ),
-    re.compile( "^(.*)/[^/]+$" ), # extract module path
-    re.compile( ".*" ),           # catch strange entries
-]
 
