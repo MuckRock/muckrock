709fe7755b45feb24fd2cb69f59e55d6faf877b0
Allan Lasser <lasser.allan@gmail.com>
2016-02-23 12:01:13 -0500

    extracts css to separate file

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,20 +1,19 @@
 var path = require('path')
 var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
+var ExtractText = require('extract-text-webpack-plugin')
 
 // paths are absolute to the root of the project, where we run `npm` commands
 var root = './muckrock/'
 
 module.exports = {
+    devtool: 'source-map',
     context: __dirname,
     entry: path.resolve(root + 'assets/js/index'),
     output: {
         path: path.resolve(root + 'assets/bundles/'),
         filename: '[name].js',
     },
-    plugins: [
-        new BundleTracker({filename: './muckrock/webpack-stats.json'}),
-    ],
     module: {
         loaders: [
             {
@@ -27,10 +26,14 @@ module.exports = {
             },
             {
                 test: /\.scss?$/,
-                loaders: ['style', 'css', 'sass'],
+                loader: ExtractText.extract('style-loader', 'css-loader!sass-loader'),
             },
         ],
     },
+    plugins: [
+        new BundleTracker({filename: './muckrock/webpack-stats.json'}),
+        new ExtractText('[name].css'),
+    ],
     resolve: {
         moduleDirectories: ['node_modules'],
         extensions: ['', '.js', '.jsx']
