67bbf512ea93ef6ffbf8e148c558e928e14cb3d1
Mitchell Kotler <mitch@mitchellkotler.net>
2016-02-27 16:06:34 -0500

    merge migrations

---
+++ muckrock/foia/migrations/0016_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('foia', '0015_foiarequest_location'),
+        ('foia', '0015_merge'),
+    ]
+
+    operations = [
+    ]
---
+++ muckrock/project/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('project', '0005_auto_20160215_0856'),
+        ('project', '0004_auto_20160224_1447'),
+    ]
+
+    operations = [
+    ]
