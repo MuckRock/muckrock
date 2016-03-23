f89e6396e83099fc47404d3c639582bcd870c7fa
Allan Lasser <lasser.allan@gmail.com>
2016-03-23 12:59:05 -0400

    refactors dropdown style and logic
    decouples dropdown from communication,
    allowing it to be reused other places

---
+++ muckrock/assets/js/dropdown.js
@@ -0,0 +1,17 @@
+var dropdowns = $('.dropdown');
+
+$('.dropdown .icon').click(function(){
+    var thisDropdown = $(this).closest('.dropdown');
+    var thisDropdownState = thisDropdown.hasClass('visible');
+    // Remove visible to all dropdowns, then make this dropdown visible
+    // if it was hidden before. If it was visible already, keep it hidden.
+    dropdowns.removeClass('visible');
+    if (!thisDropdownState) {
+        thisDropdown.addClass('visible');
+    }
+    // If we click anywhere else on the document, the dropdown should hide.
+    $(document).click(function(e){
+        dropdowns.removeClass('visible');
+    });
+    return false;
+});
