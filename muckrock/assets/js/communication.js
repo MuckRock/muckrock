690ed92cda29ee876e8d3063af0cbf0322a104af
Allan Lasser <lasser.allan@gmail.com>
2016-04-07 17:17:18 -0400

    removes inline js from communication

--- muckrock/assets/js/communication.js
+++ muckrock/assets/js/communication.js
@@ -1,10 +1,14 @@
-export function showCommForm(selector) {
+function showCommForm(id) {
+    if (!id) {
+        return;
+    }
+    var form = $(id);
     $('.options.dropdown').removeClass('visible');
     $('.communication-actions').show();
-    $(selector).addClass('visible').siblings().removeClass('visible');
-    $(selector).find('button.cancel').click(function(e){
+    form.addClass('visible').siblings().removeClass('visible');
+    form.find('button.cancel').click(function(e){
         e.preventDefault();
-        $(selector).removeClass('visible');
+        $(id).removeClass('visible');
         $('.communication-actions').hide();
     });
 }
@@ -17,3 +21,18 @@ $('.communication-header .dropdown-list').click(function(event){
     // Prevent click from propagating up to the communication header.
     event.stopPropagation();
 });
+
+var actions = $('.communication-action').map(function() {
+    return '#' + $(this).attr('id');
+}).get();
+
+// Bind to hashchange event
+$(window).on('hashchange', function () {
+    // check if the hash is a target
+    var hash = location.hash;
+    if (actions.indexOf(hash) !== -1) {
+        showCommForm(hash);
+    }
+});
+
+showCommForm(actions.indexOf(location.hash) !== -1 ? location.hash : '');
