8c64dcdee7a6e7f04b1407b080aef651dd215bc4
Allan Lasser <lasser.allan@gmail.com>
2016-04-14 20:15:55 -0400

    adds prosemirror

---
+++ muckrock/assets/js/editor.js
@@ -0,0 +1,21 @@
+import { ProseMirror } from 'prosemirror';
+import "prosemirror/dist/inputrules/autoinput";
+import 'prosemirror/dist/markdown';
+import 'prosemirror/dist/menu/menubar';
+
+let editor = document.querySelector('textarea.prose-editor');
+if (editor) {
+    let pm = window.pm = new ProseMirror({
+        place: function(newNode){
+            editor.parentNode.insertBefore(newNode, editor.nextSibling);
+            editor.style.display = 'none';
+        },
+        doc: editor.value,
+        autoInput: true,
+        docFormat: 'markdown',
+        menuBar: true
+    });
+    pm.on('change', function(){
+        editor.value = pm.getContent("markdown");
+    });
+}
