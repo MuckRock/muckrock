b18006db3686e12b39b4ce267637027c5186c10d
Allan Lasser <lasser.allan@gmail.com>
2016-04-26 18:50:55 -0400

    allows templates to be set as kwargs

--- muckrock/message/email.py
+++ muckrock/message/email.py
@@ -11,15 +11,15 @@ class TemplateEmail(EmailMultiAlternatives):
     The TemplateEmail class provides a base for our transactional emails.
     It supports sending a templated email to a user and providing extra template context.
     It always adds the MuckRock diagnostic email as a BCC'd address.
-    Both a HTML and a text template should be provided by subclasses.
+    Both a HTML and a text template should be provided by subclasses or instances.
     Subjects are expected to be provided at initialization, however a subclass may provide
     a static subject attribute if it is provided to the super __init__ method as as kwarg.
     """
+    user = None
     text_template = None
     html_template = None
-    user = None
 
-    def __init__(self, user=None, extra_context=None, **kwargs):
+    def __init__(self, user=None, extra_context=None, text_template=None, html_template=None, **kwargs):
         """Sets the universal attributes for all our email."""
         super(TemplateEmail, self).__init__(**kwargs)
         if isinstance(user, User):
@@ -27,6 +27,10 @@ class TemplateEmail(EmailMultiAlternatives):
             self.to.append(user.email)
         else:
             raise TypeError('"user" argument expects a User type')
+        if text_template:
+            self.text_template = text_template
+        if html_template:
+            self.html_template = html_template
         context = self.get_context_data(extra_context)
         content = {
             'text': render_to_string(self.get_text_template(), context),
