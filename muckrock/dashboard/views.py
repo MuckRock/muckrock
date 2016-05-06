13642fbc16cdd25c22025a3692364264244bcbb0
Allan Lasser <lasser.allan@gmail.com>
2016-05-06 13:20:26 -0400

    adds date filtering to statistics

--- muckrock/dashboard/views.py
+++ muckrock/dashboard/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from datetime import date, timedelta
+from dateutil.parser import parse
 
 from muckrock.accounts.models import Statistics
 
@@ -16,6 +17,14 @@ def dashboard(request):
 
 def dashboard_data(request):
     """Returns a week of statistics rendered into JSON."""
-    stats = Statistics.objects.all()[:30]
+    stats = Statistics.objects.all()
+    min_date = request.GET.get('min_date')
+    max_date = request.GET.get('max_date')
+    if max_date:
+        max_date = parse(max_date)
+        stats = stats.exclude(date__gt=max_date)
+    if min_date:
+        min_date = parse(min_date)
+        stats = stats.exclude(date__lt=min_date)
     data = serializers.serialize('json', stats)
     return HttpResponse(data, content_type='application/json')
