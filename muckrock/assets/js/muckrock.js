0bb94c869901834ac062c27ffd5b131083c04a13
Allan Lasser <lasser.allan@gmail.com>
2016-06-20 19:28:24 -0400

    refactors collapsable logic into general ruleset
    - any parent object with the 'collapsable' class is collapsable
    - the 'header' of the 'collapsable' object triggers the collapse
    - any child of the header with the 'nocollapse' class does not trigger collapse

--- muckrock/assets/js/muckrock.js
+++ muckrock/assets/js/muckrock.js
@@ -69,6 +69,15 @@ $.expr[":"].icontains = $.expr.createPseudo(function(arg) {
     };
 });
 
+// COLLAPSABLE
+$('.collapsable header').click(function(){
+    $(this).parent().toggleClass('collapsed');
+});
+$('.collapsable header').children('.nocollapse').click(function(event){
+    // Prevent click from propagating up to the collapsable header.
+    event.stopPropagation();
+});
+
 $('#sidebar-button').click(function(){
     var overlay = '#modal-overlay';
     var sidebar = '#website-sidebar';
