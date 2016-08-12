e34bc9761600afd874b26a0df76a92487b4a68bf
Allan Lasser <lasser.allan@gmail.com>
2016-08-12 14:29:09 -0400

    renders returned data into page

--- muckrock/assets/js/exemptionBrowser.js
+++ muckrock/assets/js/exemptionBrowser.js
@@ -8,7 +8,7 @@
 import _ from 'lodash';
 
 (function( $ ){
-    var $browser;
+    var $browser, globalQuery;
 
     function getFormValue(key, form) {
         // Given a form selector or target, return a value from the form
@@ -24,15 +24,43 @@ import _ from 'lodash';
         }
     }
 
-    function getQueryResults(endpoint, form) {
-        var data = $(form).serializeArray();
+    function renderResultListItem(result) {
+        var $result = $('<li class="result"></li>');
+        $result.append(result.name);
+        return $result;
+    }
+
+    function renderResultsList(results) {
+        // Renders a list of the results.
+        console.debug('Results:\t', results);
+        var $resultsList = $browser.append('<ul class="results"></ul>');
+        _(results).forEach(function(result){
+
+            $resultsList.append(renderResultListItem(result));
+        });
+    }
+
+    function renderEmptyResultsList() {
+        // Renders an empty list.
+        console.debug('No results!');
+    }
+
+    function searchSuccess(data, status, request) {
+        console.debug('Response:\t', data);
+        if (data.count > 0) {
+            renderResultsList(data.results);
+        } else {
+            renderEmptyResultsList();
+        }
+    }
+
+    function performSearch(form) {
+        // Perform the search using AJAX
         $.ajax({
-            url: endpoint,
-            type: 'GET',
-            data: data,
-            success: function(data, status, request) {
-                console.debug('Response:\t', data);
-            }
+            url: form.action,
+            type: form.method,
+            data: $(form).serializeArray(),
+            success: searchSuccess
         });
     }
 
@@ -41,11 +69,15 @@ import _ from 'lodash';
         // 1. Submit the query via AJAX
         // 2. Render the response into a list of results.
         var form = event.target;
-        var action = form.action;
         var query = getFormValue('q', form);
-        getQueryResults(action, form);
-        console.debug('Action:\t', action);
-        console.debug('Query:\t', query);
+        // Check that the query changed.
+        // If it changed, then perform the query.
+        // If it didn't change, then don't update the results!
+        if (query != globalQuery) {
+            globalQuery = query;
+            console.debug('New query:\t', query);
+            performSearch(form);
+        }
         return false;
     }
 
