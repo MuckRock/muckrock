a833287662bd87a862daa08aab90d78f3b1ac855
Allan Lasser <lasser.allan@gmail.com>
2016-08-16 17:42:42 -0400

    drops appeal language into appeal composer

--- muckrock/assets/js/exemptionBrowser/components/ExemptionDetail.jsx
+++ muckrock/assets/js/exemptionBrowser/components/ExemptionDetail.jsx
@@ -5,14 +5,26 @@
 
 import React, { PropTypes } from 'react';
 
-const AppealLanguage = ({appeal}) => (
-    <div className="exemption__detail__appeal">
-        <p>{appeal.context}</p>
-        <textarea readOnly value={appeal.language}></textarea>
-        <p className="small">Use this language in your appeal</p>
-        <button onClick={e=>{alert('do something!');}}>Sample</button>
-    </div>
-)
+const AppealLanguage = ({appeal}) => {
+    const handleClick = (e) => {
+        /*
+        We're gonna cheat here and break out of the React context. When handling the appeal button click, we're going to append the appeal language to the appeal composer box.
+
+        Hopefully we'll migrate most of the composer over to React/Redux also! But until that happens, we'll just use jQuery to pop it in.
+        */
+        const language = appeal.language;
+        const $appealComposer = $('#appeal-composer');
+        $appealComposer.val($appealComposer.val() + '\n\n' + language);
+    }
+    return (
+        <div className="exemption__detail__appeal">
+            <p>{appeal.context}</p>
+            <textarea readOnly value={appeal.language}></textarea>
+            <p className="small">Use this language in your appeal</p>
+            <button className="button" onClick={handleClick}>Sample</button>
+        </div>
+    )
+};
 
 const ExemptionDetail = ({exemption, onBackClick}) => {
     const appeals = exemption.example_appeals.map((appeal, i) => (
