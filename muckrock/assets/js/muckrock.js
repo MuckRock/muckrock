040e3a8acd4e5f9458d1d4a40735f031ead6425f
Allan Lasser <lasser.allan@gmail.com>
2016-09-06 18:13:11 -0400

    actually fixes jquery-ui
    - locks version at 1.10.x
    - the new version, 1.12.x, is NOT backwards compatable
    - see: https://jqueryui.com/upgrade-guide/1.12/#official-package-on-npm
    
    we will be able to upgrade to 1.12 once we remove JS from templates

--- muckrock/assets/js/muckrock.js
+++ muckrock/assets/js/muckrock.js
@@ -11,7 +11,7 @@
 **   standalone files and then imported back into here.
 */
 
-import 'jquery-ui';
+import 'jquery-ui/datepicker';
 import '../vendor/formset';
 import '../vendor/loupe';
 import '../vendor/quicksearch';
