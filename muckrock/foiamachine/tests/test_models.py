662d1ac08d0170e61d4462546aa9cce01ed1c013
Allan Lasser <lasser.allan@gmail.com>
2016-10-03 18:41:54 -0400

    refactors tests

---
+++ muckrock/foiamachine/tests/test_models.py
@@ -0,0 +1,89 @@
+"""
+Tests for FOIA Machine models.
+"""
+
+from django.template.loader import render_to_string
+from django.test import TestCase
+
+from django_hosts.resolvers import reverse
+from nose.tools import eq_, ok_
+
+from muckrock.factories import UserFactory, AgencyFactory
+from muckrock.foiamachine import factories, models
+
+class TestFoiaMachineRequest(TestCase):
+    """The FOIA Machine Request should store information we need to send a request."""
+    def setUp(self):
+        self.user = UserFactory()
+        self.title = 'Test Request'
+        self.request_language = 'Lorem ipsum'
+        self.agency = AgencyFactory()
+        self.jurisdiction = self.agency.jurisdiction
+        self.foi = factories.FoiaMachineRequestFactory(
+            user=self.user,
+            title=self.title,
+            request_language=self.request_language,
+            jurisdiction=self.jurisdiction,
+        )
+
+    def test_create(self):
+        """Requests should only require a user, a title,
+        request language, and a jurisdiction to be created."""
+        foi = models.FoiaMachineRequest.objects.create(
+            user=self.user,
+            title=self.title,
+            request_language=self.request_language,
+            jurisdiction=self.jurisdiction,
+        )
+        ok_(foi, 'The request should be created.')
+        ok_(foi.slug, 'The slug should be created automatically.')
+
+    def test_unicode(self):
+        """Requests should use their titles when converted to unicode."""
+        eq_(unicode(self.foi), self.foi.title,
+            'The Unicode representation should be the title.')
+
+    def test_get_absolute_url(self):
+        """Request urls should include their slug and their id."""
+        kwargs = {
+            'slug': self.foi.slug,
+            'pk': self.foi.pk,
+        }
+        actual_url = self.foi.get_absolute_url()
+        expected_url = reverse('foi-detail', host='foiamachine', kwargs=kwargs)
+        eq_(actual_url, expected_url)
+
+    def test_generate_letter(self):
+        """Using default information, the request should be able to generate a letter."""
+        template = 'text/foia/request.txt'
+        context = {
+            'jurisdiction': self.foi.jurisdiction,
+            'document_request': self.foi.request_language,
+            'user_name': self.foi.user.get_full_name()
+        }
+        expected_letter = render_to_string(template, context=context)
+        actual_letter = self.foi.generate_letter()
+        eq_(actual_letter, expected_letter)
+
+
+class TestFoiaMachineCommunication(TestCase):
+    """The FOIA Machine Communication should store information
+    about communications between users and agencies."""
+    def setUp(self):
+        self.foi = factories.FoiaMachineRequestFactory()
+        self.comm = factories.FoiaMachineCommunicationFactory(request=self.foi)
+
+    def test_create(self):
+        """A request, sender, and message should be required to create a request."""
+        comm = models.FoiaMachineCommunication(
+            request=self.foi,
+            sender=unicode(self.foi.user),
+            message='Lorem ipsum dolor su amit.'
+        )
+        ok_(comm)
+
+    def test_unicode(self):
+        """The string representation of a communication includes sender and receiver info."""
+        eq_(unicode(self.comm),
+            'Communication from %s to %s' % (self.comm.sender, self.comm.receiver))
+
--- muckrock/foiamachine/tests/test_views.py
+++ muckrock/foiamachine/tests/test_views.py
@@ -1,5 +1,5 @@
 """
-Tests for the FOIAMachine application.
+Tests for FOIA Machine views.
 """
 
 from django.contrib import auth
@@ -7,15 +7,13 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.template.loader import render_to_string
 from django.test import TestCase
 
-
 from django_hosts.resolvers import reverse
 from nose.tools import eq_, ok_, raises
 
-from muckrock.factories import UserFactory, AgencyFactory
+from muckrock.factories import UserFactory
 from muckrock.foiamachine import factories, forms, models, views
 from muckrock.jurisdiction.factories import StateJurisdictionFactory
 from muckrock.test_utils import http_get_response, http_post_response
-
 
 class TestHomepage(TestCase):
     """The homepage should provide information about FOIAMachine and helpful links."""
@@ -243,125 +241,4 @@ class TestFoiaMachineDeleteView(TestCase):
         self.foi.refresh_from_db()
 
 
-class TestFoiaMachineRequest(TestCase):
-    """The FOIA Machine Request should store information we need to send a request."""
-    def setUp(self):
-        self.user = UserFactory()
-        self.title = 'Test Request'
-        self.request_language = 'Lorem ipsum'
-        self.agency = AgencyFactory()
-        self.jurisdiction = self.agency.jurisdiction
-        self.foi = factories.FoiaMachineRequestFactory(
-            user=self.user,
-            title=self.title,
-            request_language=self.request_language,
-            jurisdiction=self.jurisdiction,
-        )
-
-    def test_create(self):
-        """Requests should only require a user, a title,
-        request language, and a jurisdiction to be created."""
-        foi = models.FoiaMachineRequest.objects.create(
-            user=self.user,
-            title=self.title,
-            request_language=self.request_language,
-            jurisdiction=self.jurisdiction,
-        )
-        ok_(foi, 'The request should be created.')
-        ok_(foi.slug, 'The slug should be created automatically.')
-
-    def test_unicode(self):
-        """Requests should use their titles when converted to unicode."""
-        eq_(unicode(self.foi), self.foi.title,
-            'The Unicode representation should be the title.')
-
-    def test_get_absolute_url(self):
-        """Request urls should include their slug and their id."""
-        kwargs = {
-            'slug': self.foi.slug,
-            'pk': self.foi.pk,
-        }
-        actual_url = self.foi.get_absolute_url()
-        expected_url = reverse('foi-detail', host='foiamachine', kwargs=kwargs)
-        eq_(actual_url, expected_url)
-
-    def test_generate_letter(self):
-        """Using default information, the request should be able to generate a letter."""
-        template = 'text/foia/request.txt'
-        context = {
-            'jurisdiction': self.foi.jurisdiction,
-            'document_request': self.foi.request_language,
-            'user_name': self.foi.user.get_full_name()
-        }
-        expected_letter = render_to_string(template, context=context)
-        actual_letter = self.foi.generate_letter()
-        eq_(actual_letter, expected_letter)
-
-
-class TestFoiaMachineRequestForm(TestCase):
-    """The FoiaMachineRequestForm provides for the creation of new requests."""
-    def setUp(self):
-        self.user = UserFactory()
-        self.title = 'Test Request'
-        self.request_language = 'Lorem ipsum'
-        self.agency = AgencyFactory()
-        self.jurisdiction = self.agency.jurisdiction
-        self.foi = factories.FoiaMachineRequestFactory(
-            user=self.user,
-            title=self.title,
-            request_language=self.request_language,
-            jurisdiction=self.jurisdiction,
-        )
-
-    def test_basic(self):
-        """A form should validate when given a title, a request, and a jurisdiction."""
-        form = forms.FoiaMachineRequestForm({
-            'title': self.title,
-            'request_language': self.request_language,
-            'jurisdiction': self.jurisdiction.id
-        })
-        ok_(form.is_valid())
-
-    def test_agency(self):
-        """The form should also accept an agency input."""
-        form = forms.FoiaMachineRequestForm({
-            'title': self.title,
-            'request_language': self.request_language,
-            'jurisdiction': self.jurisdiction.id,
-            'agency': self.agency.id,
-        })
-        ok_(form.is_valid())
-
-    def test_agency_mismatch(self):
-        """The form should not validate if the agency is from a different jurisdiction."""
-        jurisdiction = StateJurisdictionFactory()
-        form = forms.FoiaMachineRequestForm({
-            'title': self.title,
-            'request_language': self.request_language,
-            'jurisdiction': jurisdiction.id,
-            'agency': self.agency.id,
-        })
-        ok_(not form.is_valid())
-
-
-class TestFoiaMachineCommunication(TestCase):
-    """The FOIA Machine Communication should store information
-    about communications between users and agencies."""
-    def setUp(self):
-        self.foi = factories.FoiaMachineRequestFactory()
-        self.comm = factories.FoiaMachineCommunicationFactory(request=self.foi)
-
-    def test_create(self):
-        """A request, sender, and message should be required to create a request."""
-        comm = models.FoiaMachineCommunication(
-            request=self.foi,
-            sender=unicode(self.foi.user),
-            message='Lorem ipsum dolor su amit.'
-        )
-        ok_(comm)
-
-    def test_unicode(self):
-        """The string representation of a communication includes sender and receiver info."""
-        eq_(unicode(self.comm),
-            'Communication from %s to %s' % (self.comm.sender, self.comm.receiver))
 
