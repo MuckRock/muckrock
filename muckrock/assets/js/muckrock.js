a7f2bb9f110a18993798408c6274ba775aa17445
Allan Lasser <lasser.allan@gmail.com>
2016-10-24 15:56:20 -0400

    sets a sensible beginning date for our datepicker

--- muckrock/assets/js/muckrock.js
+++ muckrock/assets/js/muckrock.js
@@ -57,9 +57,9 @@ $('document').ready(function(){
     $('.datepicker').datepicker({
         changeMonth: true,
         changeYear: true,
-        minDate: new Date(1776, 6, 4),
+        minDate: new Date(2010, 0, 1),
         maxDate: '+1y',
-        yearRange: '1776:+1'
+        yearRange: '2010:+1'
     });
 
     // Loupe
