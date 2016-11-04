b27a921eac8aea1fa0590601eb698f5d118c1d86
Allan Lasser <lasser.allan@gmail.com>
2016-11-04 16:32:50 -0400

    fixes foiamachine flatpages

--- muckrock/context_processors.py
+++ muckrock/context_processors.py
@@ -6,7 +6,7 @@ from django.contrib.sites.models import Site
 def domain(request):
     """Add the domain to the context for constructing absolute urls."""
     # pylint: disable=unused-argument
-    current_site = Site.objects.get_current()
+    current_site = Site.objects.get_current(request)
     return {'domain': current_site.domain}
 
 def google_analytics(request):
--- muckrock/settings/base.py
+++ muckrock/settings/base.py
@@ -55,8 +55,6 @@ USE_TZ = False
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 1
-
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = False
