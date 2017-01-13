8c1fe4d1a01cb0de34993d502388a0c60a38af23
Mitchell Kotler <mitch@mitchellkotler.net>
2017-01-12 21:41:50 -0500

    close #1130 ability to edit crowdfund payments on admin

--- muckrock/crowdfund/admin.py
+++ muckrock/crowdfund/admin.py
@@ -2,18 +2,35 @@
 Admin registration for Crowdfunding
 """
 
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import User
 
+from autocomplete_light import shortcuts as autocomplete_light
 from reversion.admin import VersionAdmin
 
 from muckrock.crowdfund import models
 
 # pylint: disable=too-many-public-methods
 
+class CrowdfundPaymentAdminForm(forms.ModelForm):
+    """Form for crowdfund payment inline"""
+
+    user = autocomplete_light.ModelChoiceField(
+            'UserAutocomplete',
+            label='User',
+            queryset=User.objects.all(),
+            required=False)
+
+    class Meta:
+        model = models.CrowdfundPayment
+        fields = '__all__'
+
+
 class CrowdfundPaymentAdmin(admin.TabularInline):
     """Model Admin for crowdfund payment"""
+    form = CrowdfundPaymentAdminForm
     model = models.CrowdfundPayment
-    readonly_fields = ('user', 'name', 'date', 'amount', 'show')
     extra = 0
 
 
