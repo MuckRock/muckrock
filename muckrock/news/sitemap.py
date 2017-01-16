86131186a61559f7e58edbc63c0386b482ef3650
Mitchell Kotler <mitch@mitchellkotler.net>
2017-01-16 15:57:55 -0500

    optimize and fix up sitemaps

--- muckrock/news/sitemap.py
+++ muckrock/news/sitemap.py
@@ -8,15 +8,13 @@ from news_sitemaps import register, NewsSitemap
 
 from muckrock.news.models import Article
 
-class LimitSitemap(Sitemap):
-    """Limit Sitemap"""
-    limit = 500
 
-class ArticleSitemap(LimitSitemap):
+class ArticleSitemap(Sitemap):
     """Sitemap for Articles"""
 
     priority = 0.7
     changefreq = 'never'
+    limit = 500
 
     def items(self):
         """Return all news articles"""
@@ -26,6 +24,7 @@ class ArticleSitemap(LimitSitemap):
         """When was the article last modified?"""
         # pylint: disable=no-self-use
         return obj.pub_date
+
 
 class ArticleNewsSitemap(NewsSitemap):
     """Article sitemap for Google News"""
@@ -39,5 +38,9 @@ class ArticleNewsSitemap(NewsSitemap):
         """When was the article last modified?"""
         # pylint: disable=no-self-use
         return obj.pub_date
+
+    def keywords(self, obj):
+        """Keywords for the article"""
+        return ','.join(t.name for t in obj.tags.all())
 
 register(articles=ArticleNewsSitemap)
