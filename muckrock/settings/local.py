a7845b8bbbc3523d11205b729536ade0900c56e1
Mitchell Kotler <mitch@mitchellkotler.net>
2017-04-06 21:35:55 -0400

    use memcache locally to debug

--- muckrock/settings/local.py
+++ muckrock/settings/local.py
@@ -56,3 +56,42 @@ EMAIL_PORT = 1025
 QUERYCOUNT = {
         'DISPLAY_DUPLICATES': 10,
         }
+
+os.environ['MEMCACHE_SERVERS'] = '127.0.0.1'
+os.environ['MEMCACHE_USERNAME'] = ''
+os.environ['MEMCACHE_PASSWORD'] = ''
+CACHES = {
+    'default': {
+        # Use pylibmc
+        'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+
+        # Use binary memcache protocol (needed for authentication)
+        'BINARY': True,
+
+        # TIMEOUT is not the connection timeout! It's the default expiration
+        # timeout that should be applied to keys! Setting it to `None`
+        # disables expiration.
+        'TIMEOUT': None,
+
+        'OPTIONS': {
+            # Enable faster IO
+            'no_block': True,
+            'tcp_nodelay': True,
+
+            # Keep connection alive
+            'tcp_keepalive': True,
+
+            # Timeout for set/get requests
+            '_poll_timeout': 2000,
+
+            # Use consistent hashing for failover
+            'ketama': True,
+
+            # Configure failover timings
+            'connect_timeout': 2000,
+            'remove_failed': 4,
+            'retry_timeout': 2,
+            'dead_timeout': 10
+        }
+    }
+}
