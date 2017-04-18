3b54ccf39d8721e9a7ab45b1a6d7fcfcdab890a4
Mitchell Kotler <mitch@mitchellkotler.net>
2017-04-17 20:57:43 -0400

    add outbound attachment model

--- muckrock/foia/models/file.py
+++ muckrock/foia/models/file.py
@@ -101,3 +101,38 @@ class FOIAFile(models.Model):
         verbose_name = 'FOIA Document File'
         ordering = ['date']
         app_label = 'foia'
+
+
+def attachment_path(instance, filename):
+    """Generate path for attachment file"""
+    return 'outbound_attachments/%s/%d/%s' % (
+            instance.user.username,
+            instance.foia.pk,
+            filename,
+            )
+
+
+class OutboundAttachment(models.Model):
+    """An uploaded file waiting to be sent out"""
+
+    foia = models.ForeignKey(
+            'FOIARequest',
+            related_name='pending_attachments',
+            )
+    user = models.ForeignKey(
+            'auth.User',
+            related_name='pending_attachments',
+            )
+    ffile = models.FileField(
+            upload_to=attachment_path,
+            verbose_name='file',
+            max_length=255,
+            )
+    date_time_stamp = models.DateTimeField()
+
+    def __unicode__(self):
+        return 'Attachment: %s by %s for request %d' % (
+                self.ffile.name(),
+                self.user.username,
+                self.foia.pk,
+                )
