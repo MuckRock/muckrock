e3d48f0b9d68cd44f221695dfbbea78e2e84c1cc
Mitchell Kotler <mitch@mitchellkotler.net>
2018-01-10 12:20:06 -0500

    Create view for crowdsource
    Form to create a new crowdsource, including the jquery form builder and
    a formset for new data sources

--- muckrock/crowdsource/models.py
+++ muckrock/crowdsource/models.py
@@ -4,9 +4,12 @@
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils import timezone
+from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
+import json
 from pyembed.core import PyEmbed
+from pyembed.core.consumer import PyEmbedConsumerError
 
 from muckrock.crowdsource import fields
 
@@ -49,22 +52,46 @@ class Crowdsource(models.Model):
         # way to select a random record
         return self.data.exclude(responses__user=user).order_by('?').first()
 
+    def create_form(self, form_json):
+        """Create the crowdsource form from the form builder json"""
+        # XXX better error check for malformed json
+        form_data = json.loads(form_json)
+        for order, field_data in enumerate(form_data):
+            field = self.fields.create(
+                    label=field_data['label'].strip('<br>'),
+                    type=field_data['type'],
+                    help_text=field_data.get('description', ''),
+                    order=order,
+                    )
+            if 'values' in field_data and field.field.accepts_choices:
+                for choice_order, value in enumerate(field_data['values']):
+                    field.choices.create(
+                            choice=value['label'],
+                            value=value['value'],
+                            order=choice_order,
+                            )
+
 
 class CrowdsourceData(models.Model):
     """A source of data to show with the crowdsource questions"""
 
     crowdsource = models.ForeignKey(Crowdsource, related_name='data')
-    url = models.URLField(
-            max_length=255,
-            help_text='This should be an oEmbed enabled URL',
-            )
+    url = models.URLField(max_length=255, verbose_name='Data URL')
 
     def __unicode__(self):
         return u'Crowdsource Data: {}'.format(self.url)
 
     def embed(self):
         """Get the html to embed into the crowdsource"""
-        return mark_safe(PyEmbed().embed(self.url, max_height=400))
+        try:
+            # first try to get embed code from oEmbed
+            return mark_safe(PyEmbed().embed(self.url, max_height=400))
+        except PyEmbedConsumerError:
+            # fall back to a simple iframe
+            return format_html(
+                    '<iframe src="{}" width="100%" height="400px"></iframe>',
+                    self.url,
+                    )
 
 
 class CrowdsourceField(models.Model):
@@ -76,20 +103,25 @@ class CrowdsourceField(models.Model):
             max_length=10,
             choices=fields.FIELD_CHOICES,
             )
+    help_text = models.CharField(max_length=255, blank=True)
     order = models.PositiveSmallIntegerField()
 
     def __unicode__(self):
         return self.label
 
-
     def get_form_field(self):
         """Return a form field appropriate for rendering this field"""
-        field = fields.FIELD_DICT[self.type]
-        if field.accepts_choices:
-            choices = [(c.choice, c.choice) for c in self.choices.all()]
-            return field.field(label=self.label, choices=choices)
-        else:
-            return field.field(label=self.label)
+        kwargs = {'label': self.label}
+        if self.field.accepts_choices:
+            kwargs['choices'] = [(c.value, c.choice) for c in self.choices.all()]
+        if self.help_text:
+            kwargs['help_text'] = self.help_text
+        return self.field.field(**kwargs)
+
+    @property
+    def field(self):
+        """Get the crowdsource field instance"""
+        return fields.FIELD_DICT[self.type]
 
     class Meta:
         ordering = ('order',)
@@ -104,6 +136,7 @@ class CrowdsourceChoice(models.Model):
 
     field = models.ForeignKey(CrowdsourceField, related_name='choices')
     choice = models.CharField(max_length=255)
+    value = models.CharField(max_length=255)
     order = models.PositiveSmallIntegerField()
 
     def __unicode__(self):
