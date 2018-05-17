6dfd2524478127fa0474d5099e520fd25194606d
Mitchell Kotler <mitch@mitchellkotler.net>
2018-05-17 14:06:05 -0400

    Web form portal type
    Close #1389

--- muckrock/portal/portals/__init__.py
+++ muckrock/portal/portals/__init__.py
@@ -4,3 +4,4 @@
 from muckrock.portal.portals.fbi import FBIPortal
 from muckrock.portal.portals.manual import ManualPortal
 from muckrock.portal.portals.nextrequest import NextRequestPortal
+from muckrock.portal.portals.webform import WebFormPortal
