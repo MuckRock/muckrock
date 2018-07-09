f609a71e2018e2ff9551c72f72f928f9d09fc4a5
Mitchell Kotler <mitch@mitchellkotler.net>
2018-07-09 16:12:02 -0400

    cast to int

--- muckrock/foia/constants.py
+++ muckrock/foia/constants.py
@@ -11,4 +11,4 @@ COMPOSER_SUBMIT_DELAY = 35 * 60
 # allow a composer to be edited 30 minutes after it has been submitted
 COMPOSER_EDIT_DELAY = 30 * 60
 
-FOIA_CSV_CHUNK_SIZE = os.environ.get('FOIA_CSV_CHUNK_SIZE', 1000)
+FOIA_CSV_CHUNK_SIZE = int(os.environ.get('FOIA_CSV_CHUNK_SIZE', 1000))
