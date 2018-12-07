f736d4632ffd0e8796a98bb5226c448f4572b6f7
Mitchell Kotler <mitch@mitchellkotler.net>
2018-12-07 09:24:53 -0500

    fix dev database name

--- muckrock/settings/base.py
+++ muckrock/settings/base.py
@@ -461,9 +461,7 @@ PUBLICATION_TIME_ZONE = '-05:00'
 urlparse.uses_netloc.append('postgres')
 
 url = urlparse.urlparse(
-    os.environ.get(
-        'DATABASE_URL', 'postgres://vagrant@localhost/muckrock_orig'
-    )
+    os.environ.get('DATABASE_URL', 'postgres://vagrant@localhost/muckrock')
 )
 
 # Update with environment configuration.
