1b4230a8432e93c0bfe20f46930351145c9a5970
Mitchell Kotler <mitch@mitchellkotler.net>
2018-12-17 10:11:25 -0500

    wip

---
+++ muckrock/squarelet/urls.py
@@ -0,0 +1,11 @@
+"""URL mappings for squarelet app"""
+
+# Django
+from django.conf.urls import url
+
+# MuckRock
+from muckrock.squarelet import views
+
+urlpatterns = [
+    url(r'^webhook/$', views.webhook, name='squarelet-webhook'),
+]
