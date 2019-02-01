0a35e658a73616deb05173be335b8933b3a3e619
Mitchell Kotler <mitch@mitchellkotler.net>
2019-02-01 13:55:17 -0500

    rename date_update in squarelet api

--- muckrock/organization/querysets.py
+++ muckrock/organization/querysets.py
@@ -13,15 +13,15 @@ class OrganizationQuerySet(models.QuerySet):
     @transaction.atomic
     def squarelet_update_or_create(self, uuid, data):
         """Update or create records based on data from squarelet"""
-        required_fields = {'name', 'slug', 'date_update'}
+        required_fields = {'name', 'slug', 'update_on'}
         missing = required_fields - (required_fields & set(data.keys()))
         if missing:
             raise ValueError('Missing required fields: {}'.format(missing))
         # convert date_update from string to date
         # XXX error handle
-        if data['date_update'] is not None:
+        if data['update_on'] is not None:
             data['date_update'] = datetime.strptime(
-                data['date_update'], '%Y-%m-%d'
+                data['update_on'], '%Y-%m-%d'
             ).date()
 
         organization, created = self.model.objects.get_or_create(uuid=uuid)
