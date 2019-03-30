2f16fb834c174abb982ea1de4bc3a8261ac9bf60
Mitchell Kotler <mitch@mitchellkotler.net>
2019-03-30 13:46:25 -0400

    autocomplete for orgs in foia composer admin

---
+++ muckrock/organization/autocomplete_light_registry.py
@@ -0,0 +1,20 @@
+"""
+Autocomplete registry for Organizations
+"""
+
+# Third Party
+from autocomplete_light import shortcuts as autocomplete_light
+
+# MuckRock
+from muckrock.organization.models import Organization
+
+autocomplete_light.register(
+    Organization,
+    name='OrganizationAutocomplete',
+    choices=Organization.objects.all(),
+    search_fields=('name',),
+    attrs={
+        'placeholder': 'Search for organizations',
+        'data-autocomplete-minimum-characters': 1,
+    }
+)
