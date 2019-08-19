d612e9ed01ecd8a4de4ed4479b298848c36cc011
Mitchell Kotler <mitch@mitchellkotler.net>
2019-08-19 13:37:02 -0400

    let staff set constance config

--- muckrock/settings/base.py
+++ muckrock/settings/base.py
@@ -618,6 +618,7 @@ DASHING = {
 }
 
 CONSTANCE_BACKEND = 'constance.backends.database.DatabaseBackend'
+CONSTANCE_SUPERUSER_ONLY = False
 CONSTANCE_CONFIG = OrderedDict([
     ('ENABLE_FOLLOWUP', (True, 'Enable automated followups')),
     (
