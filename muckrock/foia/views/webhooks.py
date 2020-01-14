d84e0afd84d5b38fc7dd4d0a09dd1b44afcc153a
Mitchell Kotler <mitch@mitchellkotler.net>
2020-01-14 09:06:30 -0500

    fix unicode lob webhook

--- muckrock/foia/views/webhooks.py
+++ muckrock/foia/views/webhooks.py
@@ -70,7 +70,7 @@ def _validate_lob(signature, timestamp, body):
     """Verify the message is from Lob"""
     digest = hmac.new(
         key=settings.LOB_WEBHOOK_KEY.encode('utf-8'),
-        msg=u'{}.{}'.format(timestamp, body).encode('utf-8'),
+        msg='{}.{}'.format(timestamp, body),
         digestmod=hashlib.sha256,
     ).hexdigest()
     match = hmac.compare_digest(signature, digest)
