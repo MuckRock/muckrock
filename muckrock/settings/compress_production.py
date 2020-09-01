25f905e96ac45bd6dabf525ad7ab1221ad2d7a34
Mitchell Kotler <mitch@mitchellkotler.net>
2020-09-01 11:40:43 -0400

    add production compress settings back

---
+++ muckrock/settings/compress_production.py
@@ -0,0 +1,12 @@
+"""
+Settings for compressing production assets
+"""
+
+# pylint: disable=wildcard-import
+# pylint: disable=unused-wildcard-import
+
+# MuckRock
+from muckrock.settings.base import *
+
+STATIC_URL = "https://cdn.muckrock.com/"
+COMPRESS_ENABLED = True
