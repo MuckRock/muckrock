c5928432b1db82ffec0521557805a2c910cb6cfe
Peter Andringa <peter.andringa@washpost.com>
2021-03-31 15:11:40 -0400

    fix: fixes per @mitchelljkotler's comments

--- muckrock/communication/importers.py
+++ muckrock/communication/importers.py
@@ -41,7 +41,7 @@ p_zip = re.compile(r"^\d{5}(?:-\d{4})?$")
 def import_addresses(file_name):
     """Import addresses from spreadsheet"""
     # pylint: disable=too-many-locals
-    s3_path = f"s3://{settings.AWS_STORAGE_BUCKET_NAME}/tmp/muckrock-addresses-tmp.csv"
+    s3_path = f"s3://{settings.AWS_STORAGE_BUCKET_NAME}/{file_name}"
     with smart_open(s3_path) as tmp_file:
         reader = csv.reader(tmp_file)
         # discard header row
