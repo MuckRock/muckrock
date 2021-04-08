5caa40c2221dc9e4b04302f82f1c29bf7ace92a8
Simon Glenn-Gregg <simon.glenn-gregg@washpost.com>
2021-04-08 17:01:44 -0400

    added flower, set celery to staging settings
    Signed-off-by: Simon Glenn-Gregg <simon.glenn-gregg@washpost.com>

--- muckrock/settings/staging.py
+++ muckrock/settings/staging.py
@@ -19,19 +19,8 @@ BANDIT_EMAIL = os.environ.get("BANDIT_EMAIL", "staging@muckrock.com")
 BANDIT_WHITELIST = [
     e.strip() for e in os.environ.get("BANDIT_WHITELIST", "").split(",") if e.strip()
 ]
-# I was being redirected to SSL locally so I disabled
-SECURE_SSL_REDIRECT = False
 
-# got some warning locally about compression, this can probably be killed after you sort out the prod/staging/local stuff
-COMPRESS_ENABLED = False
-COMPRESS_OFFLINE = False
-
-
-class HijackMailgunBackend(HijackBackendMixin, MailgunBackend):
-    """This backend hijacks all emails and sends them via Mailgun"""
-
-
-EMAIL_BACKEND = "muckrock.settings.staging.HijackMailgunBackend"
+EMAIL_BACKEND = "bandit.backends.smtp.HijackSMTPBackend"
 
 # set proxy for static outgoing IP address, so we can cross
 # white list muckrock and squarelet staging sites
