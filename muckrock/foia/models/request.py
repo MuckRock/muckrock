7ffd921d543b9a54890ed3cfbc3feea837c5ad5b
Simon Glenn-Gregg <simon.glenn-gregg@washpost.com>
2021-04-10 11:31:41 -0400

    switch EMAIL_BACKEND settings syntax, disable silk profiler locally
    Signed-off-by: Simon Glenn-Gregg <simon.glenn-gregg@washpost.com>

--- muckrock/foia/models/request.py
+++ muckrock/foia/models/request.py
@@ -779,9 +779,8 @@ class FOIARequest(models.Model):
 
         # if we are using celery email, we want to not use it here, and use the
         # celery email backend directly.  Otherwise just use the default email backend
-        backend = "django_mailgun.MailgunBackend"
-        logger.info("email backend from settings: %s", settings.EMAIL_BACKEND)
-        logger.info("backend hardcoded: %s", backend)
+        backend = getattr(settings, "CELERY_EMAIL_BACKEND", settings.EMAIL_BACKEND)
+        logger.info("email backend: %s", backend)
         with get_connection(backend) as email_connection:
             msg = EmailMultiAlternatives(
                 subject=comm.subject,
@@ -798,6 +797,7 @@ class FOIARequest(models.Model):
             comm.attach_files_to_email(msg)
 
             try:
+                logger.info("sending mail")
                 msg.send(fail_silently=False)
             except MailgunAPIError as exc:
                 EmailError.objects.create(
