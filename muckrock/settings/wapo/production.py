8d238ebdba4e4ddbec5c5204e271c2d2898ff543
Simon Glenn-Gregg <simon.glenn-gregg@washpost.com>
2021-04-26 12:35:24 -0400

    add prod refresh config, ALLOWED_HOSTS
    Signed-off-by: Simon Glenn-Gregg <simon.glenn-gregg@washpost.com>

--- muckrock/settings/wapo/production.py
+++ muckrock/settings/wapo/production.py
@@ -13,3 +13,12 @@ BANDIT_EMAIL = os.environ.get("BANDIT_EMAIL", "staging@muckrock.com")
 BANDIT_WHITELIST = [
     e.strip() for e in os.environ.get("BANDIT_WHITELIST", "").split(",") if e.strip()
 ]
+
+# This gets the IP address of the EC2 instance the task is
+# running on and adds it to allowed_hosts so the health
+# check will work
+try:
+    EC2_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4').text
+    ALLOWED_HOSTS.append(EC2_IP)
+except requests.exceptions.RequestException:
+    pass
