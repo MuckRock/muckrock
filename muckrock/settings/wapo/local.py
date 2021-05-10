30da9f9c03b9a1fbb0559df3a7abbc13b0376efb
Simon Glenn-Gregg <simon.glenn-gregg@washpost.com>
2021-05-10 13:32:52 -0400

    revert wapo changes to compose/ pip/ and added debugging/healthchecks, remove wapo-specific directories
    Signed-off-by: Simon Glenn-Gregg <simon.glenn-gregg@washpost.com>

--- muckrock/settings/wapo/local.py
+++
@@ -1,8 +0,0 @@
-from muckrock.settings.local import *
-from muckrock.settings.wapo.base import *
-
-AWS_STORAGE_BUCKET_NAME="wp-muckrock-dev"
-AWS_MEDIA_BUCKET_NAME="wp-muckrock-uploads-dev"
-CLOUDFRONT_DOMAIN="wp-muckrock-dev.news-engineering.aws.wapo.pub"
-AWS_STORAGE_DEFAULT_ACL="private"
-AWS_MEDIA_QUERYSTRING_AUTH=True
