32e68c9ca924bd8f93bde34b80d9b9933287071c
Simon Glenn-Gregg <simon.glenn-gregg@washpost.com>
2021-05-10 13:32:52 -0400

    revert wapo changes to compose/ pip/ and added debugging/healthchecks, remove wapo-specific directories
    Signed-off-by: Simon Glenn-Gregg <simon.glenn-gregg@washpost.com>

--- muckrock/core/celery.py
+++ muckrock/core/celery.py
@@ -1,18 +1,17 @@
 """Celery configuration app"""
 # Django
-from celery import Celery, signals
+from celery import Celery
 from django.conf import settings
 
 # Standard Library
-import os, logging
+import os
 
 # Third Party
 import scout_apm.celery
 
 # set the default Django settings module for the 'celery' program.
-#  os.environ.setdefault("DJANGO_SETTINGS_MODULE", "muckrock.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "muckrock.settings.local")
 
-logger = logging.getLogger(__name__)
 app = Celery("muckrock")
 
 # Using a string here means the worker doesn't have to serialize
@@ -26,18 +25,3 @@ app.autodiscover_tasks()
 
 if settings.USE_SCOUT:
     scout_apm.celery.install(app)
-
-@signals.task_retry.connect
-@signals.task_failure.connect
-@signals.task_revoked.connect
-def on_task_failure(**kwargs):
-    """Abort transaction on task errors.
-    """
-    # celery exceptions will not be published to `sys.excepthook`. therefore we have to create another handler here.
-    from traceback import format_tb
-
-    logger.error('[task:%s:%s]' % (kwargs.get('task_id'), kwargs['sender'].request.correlation_id, )
-              + '\n'
-              + ''.join(format_tb(kwargs.get('traceback', [])))
-              + '\n'
-              + str(kwargs.get('exception', '')))
--- wapo-management/null-fixtures.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# recursively find/replace foreign key references in a batch of fixture files
-
-find fixtures -name *.json | xargs sed -iE '.bak' s/'"form":\ [0-9]..*/\"form\":\ null,'/g
-
-# this generates copies of your old files with .bak extensions, run this to cleanup:
-
-# rm fixtures/*.bak
