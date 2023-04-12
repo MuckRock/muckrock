a35860a22c347c6b721db7883b40334e431b3cf6
Mitchell Kotler <mitch@mitchellkotler.net>
2023-04-12 08:55:47 -0400

    initial foia logs

--- muckrock/foia/models/__init__.py
+++ muckrock/foia/models/__init__.py
@@ -7,6 +7,7 @@ from muckrock.foia.models.attachment import *
 from muckrock.foia.models.communication import *
 from muckrock.foia.models.composer import *
 from muckrock.foia.models.file import *
+from muckrock.foia.models.log import *
 from muckrock.foia.models.multirequest import *
 from muckrock.foia.models.request import *
 from muckrock.foia.models.search import *
