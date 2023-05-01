2d877bb16b37e9012cf33c41bcf208ae89ad32e7
Mitchell Kotler <mitch@mitchellkotler.net>
2023-05-01 09:51:23 -0400

    fix settings

--- muckrock/settings/compress_production.py
+++ muckrock/settings/compress_production.py
@@ -8,8 +8,11 @@ Settings for compressing production assets
 # MuckRock
 from muckrock.settings.base import *
 
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+STORAGES["staticfiles"][
+    "BACKEND"
+] = "django.contrib.staticfiles.storage.StaticFilesStorage"
 COMPRESS_STORAGE = "compressor.storage.CompressorFileStorage"
+STORAGES["compressor"]["BACKEND"] = COMPRESS_STORAGE
 STATIC_URL = "https://cdn.muckrock.com/"
 COMPRESS_URL = STATIC_URL
 COMPRESS_ENABLED = True
