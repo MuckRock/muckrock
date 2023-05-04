bf3ca77286e631492c38aaea80434d23ac62337d
Allan Lasser <lasser.allan@gmail.com>
2023-05-04 14:30:59 -0400

    Updates to Webpack 5

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,40 +1,55 @@
 var path = require('path')
 var webpack = require('webpack')
-var validate = require('webpack-validator')
+// var validate = require('webpack-validator')
 var BundleTracker = require('webpack-bundle-tracker')
-var ExtractText = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 // paths are absolute to the root of the project, where we run `npm` commands
 var root = './muckrock/'
 
 var config = {
+    mode: 'development',
     devtool: 'source-map',
     entry: {
         main: path.resolve(root + 'assets/entry'),
         foiamachine: path.resolve(root + 'foiamachine/assets/entry'),
-        docViewer: path.resolve(root + 'assets/js/docViewer'),
+        docViewer: path.resolve(root + 'assets/js/docViewer')
     },
     output: {
         path: path.resolve(root + 'assets/bundles/'),
-        filename: '[name].js',
+        filename: '[name].js'
     },
+    plugins: [
+        new BundleTracker({filename: './muckrock/assets/webpack-stats.json'}),
+        new MiniCssExtractPlugin(),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+        })
+    ],
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react'],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
                 }
             },
             {
                 test: /\.scss?$/,
-                loader: ExtractText.extract('style-loader', 'css-loader?sourceMap!sass-loader?sourceMap'),
+                use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap']
             },
             {
                 test: /jquery\.js$/,
-                loader: 'expose?jQuery!expose?$'
+                use: ['expose?jQuery', 'expose'],
             },
             {
                 test: /\.json$/,
@@ -46,24 +61,12 @@ var config = {
             },
         ],
     },
-    plugins: [
-        new BundleTracker({filename: './muckrock/assets/webpack-stats.json'}),
-        new ExtractText('[name].css'),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            "window.jQuery": "jquery"
-        }),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-        })
-    ],
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
     watchOptions: {
-	    poll: true,
-    },
+        poll: true
+    }
 };
 
-module.exports = validate(config);
+module.exports = config;
