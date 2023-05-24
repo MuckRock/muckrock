e47827dcbb41207caa2a8dd6926e35faeceb9582
Mitchell Kotler <mitch@mitchellkotler.net>
2023-05-24 10:59:40 -0400

    fix s3 path

--- muckrock/portal/portals/govqa.py
+++ muckrock/portal/portals/govqa.py
@@ -119,6 +119,7 @@ class GovQAPortal(ManualPortal):
                 )
                 return
             path = get_path(file_name)
+            path = f"s3://{{settings.AWS_MEDIA_BUCKET_NAME}/{path}"
             with smart_open(path, "wb") as file:
                 for chunk in resp.iter_content(chunk_size=10 * 1024 * 1024):
                     file.write(chunk)
