bee3e62f8c2fc16b8f30d9e35c0a6022978c2670
Allan Lasser <lasser.allan@gmail.com>
2023-06-28 14:12:02 -0400

    Updates FOIA log list configs

--- muckrock/foia/views/foia_log.py
+++ muckrock/foia/views/foia_log.py
@@ -4,9 +4,11 @@ Detail view for a FOIA Log
 
 # Django
 from django.views.generic import DetailView
+from muckrock.core.views import ModelFilterMixin, MRListView
 
 # MuckRock
 from muckrock.foia.models.log import FOIALog
+from muckrock.foia.filters import FOIALogFilterSet
 
 class FOIALogDetail(DetailView):
     """Details of a single FOIA Log"""
@@ -15,3 +17,13 @@ class FOIALogDetail(DetailView):
     context_object_name = "foia_log"
     pk_url_kwarg = "idx"
     template_name = "foia/foia_log/detail.html"
+
+class FOIALogList(ModelFilterMixin, MRListView):
+    """Filterable list of FOIA logs"""
+
+    model = FOIALog
+    template_name = "foia/foia_log/list.html"
+    foia = None
+    filter_class = FOIALogFilterSet
+    title = "FOIA Logs"
+    
