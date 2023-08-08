5055707bc717cdd9121b9c0beac32e6049807565
Mitchell Kotler <mitch@mitchellkotler.net>
2023-08-08 13:07:41 -0400

    let staff bulk embargo and no index requests

--- muckrock/foia/views/list.py
+++ muckrock/foia/views/list.py
@@ -6,6 +6,7 @@ Views to display lists of FOIA requests
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.db import transaction
 from django.db.models import Count, Prefetch
 from django.http import Http404
 from django.shortcuts import redirect
@@ -46,7 +47,7 @@ from muckrock.foia.models import (
     FOIASavedSearch,
 )
 from muckrock.foia.rules import can_embargo, can_embargo_permananently
-from muckrock.foia.tasks import export_csv
+from muckrock.foia.tasks import export_csv, noindex_documentcloud
 from muckrock.news.models import Article
 from muckrock.project.forms import ProjectManagerForm
 from muckrock.project.models import Project
@@ -240,6 +241,8 @@ class RequestList(MRSearchFilterListView):
         if self.request.user.is_staff:
             actions["review-agency"] = self._review_agency
             actions["change-owner"] = self._change_owner
+            actions["embargo"] = self._embargo
+            actions["noindex"] = self._noindex
         return actions
 
     def _delete(self, request):
@@ -332,6 +335,19 @@ class RequestList(MRSearchFilterListView):
             )
         else:
             return None
+
+    def _embargo(self, foias, user, post):
+        """Embargo the requests"""
+        foias.update(embargo=True, permanent_embargo=True)
+        return "Requests have been embargoed"
+
+    def _noindex(self, foias, user, post):
+        """No index the requests"""
+        with transaction.atomic():
+            foias.update(noindex=True)
+            for foia in foias:
+                transaction.on_commit(lambda f=foia: noindex_documentcloud.delay(f.pk))
+        return "Requests have been no-indexed"
 
     def get(self, request, *args, **kwargs):
         """Check for loading saved searches"""
