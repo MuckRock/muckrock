a5be99e24833231ab5604c5e05172e168cf7273c
Mitchell Kotler <mitch@mitchellkotler.net>
2023-10-19 15:01:40 -0400

    Update FOIA Log Entries to be grouped by Log

--- muckrock/foia/views/logs.py
+++ muckrock/foia/views/logs.py
@@ -10,10 +10,10 @@ from django.views.generic.edit import FormView
 # MuckRock
 from muckrock.agency.models.agency import Agency
 from muckrock.core.views import MRSearchFilterListView
-from muckrock.foia.filters import FOIALogFilterSet
+from muckrock.foia.filters import FOIALogEntryFilterSet
 from muckrock.foia.forms.logs import FOIALogUploadForm
 from muckrock.foia.importers import import_logs
-from muckrock.foia.models.log import FOIALog
+from muckrock.foia.models.log import FOIALogEntry
 
 
 class FOIALogUploadView(UserPassesTestMixin, FormView):
@@ -32,7 +32,7 @@ class FOIALogUploadView(UserPassesTestMixin, FormView):
 
     def form_valid(self, form):
         """Import on succesful upload"""
-        num = import_logs(form.cleaned_data["agency"], form.cleaned_data["log"])
+        num = import_logs(form, self.request.user)
         messages.success(self.request, f"Import succesful - {num} logs imported")
         return super().form_valid(form)
 
@@ -40,7 +40,7 @@ class FOIALogUploadView(UserPassesTestMixin, FormView):
 class FOIALogDetail(DetailView):
     """Details of a single FOIA Log"""
 
-    model = FOIALog
+    model = FOIALogEntry
     context_object_name = "foia_log"
     pk_url_kwarg = "idx"
     template_name = "foia/foia_log/detail.html"
@@ -49,17 +49,17 @@ class FOIALogDetail(DetailView):
 class FOIALogList(MRSearchFilterListView):
     """Filterable list of FOIA logs"""
 
-    model = FOIALog
+    model = FOIALogEntry
     template_name = "foia/foia_log/list.html"
     foia = None
-    filter_class = FOIALogFilterSet
+    filter_class = FOIALogEntryFilterSet
     title = "FOIA Logs"
     default_sort = "date_requested"
     default_order = "desc"
 
     def get_queryset(self):
         """Select related agency and jurisdiction to avoid extra queries"""
-        return super().get_queryset().select_related("agency__jurisdiction")
+        return super().get_queryset().select_related("foia_log__agency__jurisdiction")
 
 
 class FOIALogLanding(TemplateView):
@@ -71,7 +71,7 @@ class FOIALogLanding(TemplateView):
         """Adds educational content and suggested searches"""
         context = super().get_context_data(**kwargs)
         context["stats"] = {
-            "log_count": FOIALog.objects.count(),
+            "log_count": FOIALogEntry.objects.count(),
             "agency_count": Agency.objects.with_logs().count(),
         }
         return context
