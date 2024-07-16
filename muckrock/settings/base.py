4f5cfad4027a3123aff8dc8dbb0388fea1faba67
Mitchell Kotler <mitch@mitchellkotler.net>
2024-07-16 15:49:46 -0400

    compress settings

--- muckrock/settings/base.py
+++ muckrock/settings/base.py
@@ -122,12 +122,14 @@ STORAGES = {
     "compressor": {
         "BACKEND": "muckrock.core.storage.CachedS3Boto3Storage",
     },
-    "compressor-offine": {
-        "BACKEND": "muckrock.core.storage.CachedS3Boto3Storage",
+    "compressor-offline": {
+        "BACKEND": "muckrock.core.storage.OfflineManifestFileStorage",
     },
 }
 THUMBNAIL_DEFAULT_STORAGE = STORAGES["default"]["BACKEND"]
 THUMBNAIL_STORAGE = STORAGES["default"]["BACKEND"]
+COMPRESS_STORAGE = STORAGES["compressor"]["BACKEND"]
+COMPRESS_OFFLINE_MANIFEST_STORAGE = STORAGES["compressor-offline"]["BACKEND"]
 CLEAN_S3_ON_FOIA_DELETE = True
 
 # Settings for static bucket storage
