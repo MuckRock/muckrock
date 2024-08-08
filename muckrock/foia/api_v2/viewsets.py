5c81d2a5fe27de4b1dd86b333b3f4a5c74e360f5
Mitchell Kotler <mitch@mitchellkotler.net>
2024-08-08 16:10:58 -0400

    initial api v2

---
+++ muckrock/foia/api_v2/viewsets.py
@@ -0,0 +1,29 @@
+"""
+Viewsets for V2 of the FOIA API
+"""
+
+# Third Party
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+# MuckRock
+from muckrock.foia.api_v2.serializers import FOIARequestSerializer
+from muckrock.foia.models.request import FOIARequest
+
+
+class FOIARequestViewSet(viewsets.ModelViewSet):
+    """API for FOIA Requests"""
+
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
+    serializer_class = FOIARequestSerializer
+    filter_backends = ()
+
+    def get_queryset(self):
+        return (
+            FOIARequest.objects.get_viewable(self.request.user)
+            .select_related("composer")
+            .prefetch_related(
+                "edit_collaborators", "read_collaborators", "tracking_ids"
+            )
+        )
