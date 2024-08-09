58e0a5c503591482aa94272c0ed27e4359dfeadf
Mitchell Kotler <mitch@mitchellkotler.net>
2024-08-09 16:27:50 -0400

    add foia communications to api v2

--- muckrock/foia/api_v2/viewsets.py
+++ muckrock/foia/api_v2/viewsets.py
@@ -3,13 +3,18 @@ Viewsets for V2 of the FOIA API
 """
 
 # Third Party
+import django_filters
+from django_filters.rest_framework.backends import DjangoFilterBackend
 from rest_framework import mixins, viewsets
 from rest_framework.authentication import SessionAuthentication
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 # MuckRock
-from muckrock.foia.api_v2.serializers import FOIARequestSerializer
-from muckrock.foia.models.request import FOIARequest
+from muckrock.foia.api_v2.serializers import (
+    FOIACommunicationSerializer,
+    FOIARequestSerializer,
+)
+from muckrock.foia.models import FOIACommunication, FOIARequest
 
 
 class FOIARequestViewSet(
@@ -29,3 +34,39 @@ class FOIARequestViewSet(
                 "edit_collaborators", "read_collaborators", "tracking_ids"
             )
         )
+
+
+class FOIACommunicationViewSet(
+    mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet
+):
+    """API for FOIA Communications"""
+
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
+    serializer_class = FOIACommunicationSerializer
+    filter_backends = (DjangoFilterBackend,)
+
+    def get_queryset(self):
+        return FOIACommunication.objects.get_viewable(self.request.user)
+
+    class Filter(django_filters.FilterSet):
+        """API Filter for FOIA Communications"""
+
+        min_date = django_filters.DateFilter(
+            field_name="datetime",
+            lookup_expr="gte",
+            label="Filter communications after this date",
+        )
+        max_date = django_filters.DateFilter(
+            field_name="datetime",
+            lookup_expr="lte",
+            label="Filter communications before this date",
+        )
+        foia = django_filters.NumberFilter(
+            field_name="foia__id", label="The ID of the associated request"
+        )
+
+        class Meta:
+            model = FOIACommunication
+            fields = ("max_date", "min_date", "foia", "status", "response")
+
+    filterset_class = Filter
