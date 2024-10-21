f20b2e0756f16890ec9dcc0cffece2732abb1244
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2024-10-21 11:49:52 -0500

    Add sample documentation for jurisdictions

--- muckrock/jurisdiction/api_v2/serializers.py
+++ muckrock/jurisdiction/api_v2/serializers.py
@@ -2,17 +2,69 @@
 
 from rest_framework import serializers
 from muckrock.jurisdiction.models import Jurisdiction
+from drf_spectacular.utils import (
+    OpenApiExample,
+    extend_schema_serializer,
+)
 
-#pylint: disable=too-few-public-methods
+
+@extend_schema_serializer(
+    examples=[
+        OpenApiExample(
+            "State Jurisdiction Example",
+            value={
+                "id": 1,
+                "name": "California",
+                "slug": "california",
+                "abbrev": "CA",
+                "level": "s",
+                "parent": 3,
+            },
+            description="Example of a state jurisdiction under the USA.",
+        ),
+        OpenApiExample(
+            "Local Jurisdiction Example",
+            value={
+                "id": 2,
+                "name": "Los Angeles",
+                "slug": "los-angeles",
+                "abbrev": "LA",
+                "level": "l",
+                "parent": 1,
+            },
+            description="Example of a local jurisdiction under California.",
+        ),
+        OpenApiExample(
+            "Federal Jurisdiction Example",
+            value={
+                "id": 3,
+                "name": "United States of America",
+                "slug": "united-states-of-america",
+                "abbrev": "USA",
+                "level": "f",
+                "parent": None,
+            },
+            description="Example of a federal jurisdiction.",
+        ),
+    ]
+)
+# pylint: disable=too-few-public-methods
 class JurisdictionSerializer(serializers.ModelSerializer):
     """Serializer for Jurisdiction model"""
 
     parent = serializers.PrimaryKeyRelatedField(
-        queryset=Jurisdiction.objects.order_by(), style={"base_template": "input.html"}
+        queryset=Jurisdiction.objects.order_by(),
+        style={"base_template": "input.html"},
+        help_text=(
+            "Parent jurisdiction. This defines the hierarchy between jurisdictions, "
+            "where a jurisdiction can have a federal or state parent. "
+            "Local jurisdictions cannot be parents."
+        )
     )
 
     class Meta:
-        """ Fields in Jurisdiction object """
+        """Fields in Jurisdiction object"""
+
         model = Jurisdiction
         fields = (
             "id",
