b42d013c87c33297914bab4d0eab8e7082ff21cd
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2024-10-22 13:05:25 -0500

    Refactor accounts and fix serializer imports on agency and jurisdiction

--- muckrock/accounts/api_v2/viewsets.py
+++ muckrock/accounts/api_v2/viewsets.py
@@ -7,22 +7,22 @@ from django.contrib.auth.models import User
 # Third Party
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
 # MuckRock
-from muckrock.accounts.serializers import UserSerializer
+from muckrock.accounts.api_v2.serializers import UserSerializer
 
 # pylint: disable=too-few-public-methods
 class UserFilter(django_filters.FilterSet):
     """ User filters """
     full_name = django_filters.CharFilter(field_name='profile__full_name', lookup_expr='icontains')
     username = django_filters.CharFilter(lookup_expr='icontains')
-    state = django_filters.CharFilter(field_name='profile__state', lookup_expr='icontains')
+    uuid = django_filters.CharFilter(field_name='profile__uuid', lookup_expr='icontains')
+    email = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         """ Fields """
         model = User
-        fields = ['full_name', 'username', 'state']
+        fields = ['full_name', 'username', 'uuid', 'email']
 
 class UserViewSet(viewsets.ModelViewSet):
     """API views for users"""
@@ -44,16 +44,3 @@ class UserViewSet(viewsets.ModelViewSet):
         if self.kwargs["pk"] == "me" and self.request.user.is_authenticated:
             return self.request.user  # Return the current user
         return super().get_object()
-
-    def list(self, request, *args, **kwargs):
-        """Show relevant users based on permissions."""
-        if request.user.is_authenticated:
-            if request.user.is_staff:
-                return super().list(request, *args, **kwargs)  # Staff can see all
-            # Non-staff users see their own info and organization members
-            queryset = self.get_queryset()  # Get organization members
-            serializer = self.get_serializer(queryset, many=True)
-            return Response(serializer.data)
-        return Response(status=403)  # Forbid unauthenticated users
-
-    filterset_fields = ("username", "profile__full_name", "email", "is_staff")
