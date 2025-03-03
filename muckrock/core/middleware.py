04f52c787148cf39e6bd90c837b032a26bcc2d7b
Allan Lasser <lasser.allan@gmail.com>
2025-03-03 08:21:54 -0500

    Handles redirects for multiple sites

--- muckrock/core/middleware.py
+++ muckrock/core/middleware.py
@@ -6,8 +6,11 @@ from django.conf import settings
 class FlatpageRedirectMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         if response.status_code == 404:
+            site = request.get_host()
             path = request.path_info.lstrip('/')
             redirects = getattr(settings, 'FLATPAGES_REDIRECTS', {})
-            if path in redirects:
-                return HttpResponsePermanentRedirect(redirects[path])
+            if site in redirects:
+                site_redirects = redirects[site]
+                if path in site_redirects:
+                    return HttpResponsePermanentRedirect(site_redirects[path])
         return response
