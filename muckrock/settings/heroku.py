362e9de0cba251d7ad4dca0bf66c31b051c8379b
Mitchell Kotler <mitch@mitchellkotler.net>
2025-08-08 13:57:46 -0400

    remove pylibmc

--- muckrock/settings/heroku.py
+++ muckrock/settings/heroku.py
@@ -23,47 +23,18 @@ TEMPLATES[0]["OPTIONS"]["loaders"] = [
 ]
 del TEMPLATES[0]["APP_DIRS"]
 
-if "MEMCACHIER_SERVERS" in os.environ:
-    servers = os.environ.get("MEMCACHIER_SERVERS", "").replace(",", ";")
-    os.environ["MEMCACHE_SERVERS"] = servers
-    username = os.environ.get("MEMCACHIER_USERNAME", "")
-    os.environ["MEMCACHE_USERNAME"] = username
-    password = os.environ.get("MEMCACHIER_PASSWORD", "")
-    os.environ["MEMCACHE_PASSWORD"] = password
-
-    CACHES["default"] = {
-        # Use pylibmc
-        "BACKEND": "django.core.cache.backends.memcached.PyLibMCCache",
-        # TIMEOUT is not the connection timeout! It's the default expiration
-        # timeout that should be applied to keys! Setting it to `None`
-        # disables expiration.
-        "TIMEOUT": None,
-        "LOCATION": servers,
-        "BINARY": True,
-        "OPTIONS": {
-            # Use binary memcache protocol (needed for authentication)
-            "binary": True,
-            "username": username,
-            "password": password,
-            "behaviors": {
-                # Enable faster IO
-                "no_block": True,
-                "tcp_nodelay": True,
-                # Keep connection alive
-                "tcp_keepalive": True,
-                # Timeout settings
-                "connect_timeout": 2000,  # ms
-                "send_timeout": 750 * 1000,  # us
-                "receive_timeout": 750 * 1000,  # us
-                "_poll_timeout": 2000,  # ms
-                # Better failover
-                "ketama": True,
-                "remove_failed": 1,
-                "retry_timeout": 2,
-                "dead_timeout": 30,
-            },
-        },
-    }
+CACHES["default"] = {
+    "BACKEND": "django_redis.cache.RedisCache",
+    "LOCATION": REDIS_URL,
+    "OPTIONS": {
+        "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        # Mimicing memcache behavior.
+        # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
+        "IGNORE_EXCEPTIONS": True,
+    },
+}
+if REDIS_URL.startswith("rediss:"):
+    CACHES["default"]["OPTIONS"]["CONNECTION_POOL_KWARGS"] = {"ssl_cert_reqs": None}
 
 CONSTANCE_DATABASE_CACHE_BACKEND = "default"
 
