98d1fecc25d14bbd9631fec7f957d4a0c3db3fba
Mitchell Kotler <mitch@mitchellkotler.net>
2025-10-15 14:23:08 -0400

    add permissions digest

---
+++ muckrock/accounts/mail.py
@@ -0,0 +1,30 @@
+# Django
+from django.conf import settings
+from django.contrib.auth.models import Group, User
+
+# MuckRock
+from muckrock.message.email import TemplateEmail
+
+
+class PermissionsDigest(TemplateEmail):
+    """A digest that provides an overview of who has what permissions"""
+
+    html_template = "accounts/email/permissions.html"
+    text_template = "accounts/email/permissions.txt"
+
+    def __init__(self, **kwargs):
+        kwargs["to"] = settings.PERMISSIONS_DIGEST_EMAILS
+        kwargs["extra_context"] = self.get_context()
+        kwargs["subject"] = "Permissions Digest"
+        super().__init__(**kwargs)
+
+    def get_context(self):
+        return {
+            "superusers": User.objects.filter(is_superuser=True),
+            "staff": User.objects.filter(is_staff=True),
+            "groups": Group.objects.prefetch_related("user_set"),
+            "user_permissions": User.user_permissions.through.objects.select_related(
+                "user",
+                "permission",
+            ),
+        }
