7e7830daf21c05882f02aabd7615c55724df112f
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2026-02-12 14:09:40 -0600

    Make constants depend on environment, form dev as default django env

--- muckrock/crowdsource/constants.py
+++ muckrock/crowdsource/constants.py
@@ -1,9 +1,22 @@
 """Constants for the Crowdsourcing app"""
 
+# Django
+from django.conf import settings
+
 # Standard Library
 import re
 
-BASE = r"https?://(www|beta|staging)\.documentcloud\.org"
+if settings.ENV == "production":
+    DOCCLOUD_BASE = r"(www\.)?documentcloud\.org"
+elif settings.ENV == "staging":
+    DOCCLOUD_BASE = r"(www\.)?staging\.documentcloud\.org"
+else:  # dev or local
+    DOCCLOUD_BASE = r"(www\.)?dev\.documentcloud\.org"
 
-DOCUMENT_URL_RE = re.compile(rf"{BASE}/documents/(?P<doc_id>[0-9A-Za-z-]+)/?")
-PROJECT_URL_RE = re.compile(rf"{BASE}/projects/(?P<proj_id>[0-9]+)/?")
+# Regexes for documents and projects
+DOCUMENT_URL_RE = re.compile(
+    rf"^https://{DOCCLOUD_BASE}/documents/(?P<doc_id>[0-9A-Za-z-]+)/?$"
+)
+PROJECT_URL_RE = re.compile(
+    rf"^https://{DOCCLOUD_BASE}/projects/(?P<proj_id>[0-9A-Za-z-]+)/?$"
+)
