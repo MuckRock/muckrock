d2872cacfd69372aba7331df45a02a40a170bf1f
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2026-03-13 22:45:34 -0500

    Fix resize_pages

--- muckrock/task/pdf.py
+++ muckrock/task/pdf.py
@@ -243,28 +243,32 @@ class MailPDF(PDF):
         """Hook for subclasses to override"""
 
     def _resize_pages(self, pages):
-        """Resize the page if necessary and able"""
-        i = 0
+        """Resize the page if necessary using pypdf"""
         for page in pages:
-            i += 1
             rotated = False
-            width = page.pagedata.mediabox.width
-            height = page.pagedata.mediabox.height
-            # account for rotations
-            rotation = page.pagedata.rotation
-            if rotation is not None and rotation % 180 == 90:
+
+            # Get current dimensions
+            width = float(page.mediabox.width)
+            height = float(page.mediabox.height)
+
+            # account for rotation
+            rotation = page.get("/Rotate", 0)
+            if rotation % 180 == 90:
                 width, height = height, width
                 rotated = not rotated
+
+            # Force landscape to portrait
             if width > height:
-                page.pagedata.rotate(-90)
-                # page.transfer_rotation_to_content()
+                page.rotate(-90)  # rotate page counter-clockwise
                 width, height = height, width
                 rotated = not rotated
+
+            # Resize if necessary
             if (width, height) != (PDF_WIDTH, PDF_HEIGHT):
                 if rotated:
-                    page.pagedata.scale_to(PDF_HEIGHT, PDF_WIDTH)
+                    page.scale_to(PDF_HEIGHT, PDF_WIDTH)
                 else:
-                    page.pagedata.scale_to(PDF_WIDTH, PDF_HEIGHT)
+                    page.scale_to(PDF_WIDTH, PDF_HEIGHT)
 
     def _handle_file(self, file_, files, merger):
         """Determine if we can attach the file"""
